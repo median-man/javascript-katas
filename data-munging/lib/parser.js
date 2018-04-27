73daa4f4a5b488d7bc8dceb31ba5ce302cc439b9
John Desrosiers <johndrunner@yahoo.com>
2018-04-27 06:48:50 -0700

    Modify parser. Add #parseRows() and #removeNonDataRows()

--- data-munging/lib/parser.js
+++ data-munging/lib/parser.js
@@ -1,5 +1,18 @@
 module.exports = {
+  parseRows: function parseRows(data) {
+    const rows = data.split(/\n/g);
+    return this.removeNonDataRows(rows);
+  },
+
   parseRow(row) {
     return row.trim().split(/\s+/g);
+  },
+
+  removeNonDataRows(rows) {
+    const matchDivider = /-{3,}/;
+    const hasValues = row => !matchDivider.test(row);
+    const hasText = row => /\S/.test(row);
+    const hasData = row => hasText(row) && hasValues(row);
+    return rows.filter(hasData);
   },
 };
--- data-munging/test/parser.test.js
+++ data-munging/test/parser.test.js
@@ -22,4 +22,82 @@ describe('Parser', () => {
       );
     });
   });
+
+  describe('removeNonDataRows()', () => {
+    const testExpectedResult = (input, expected) => {
+      const actual = parser.removeNonDataRows(input);
+      const msg = `[${expected.join().trim().substr(0, 20)}...]`;
+      expect(actual).to.eql(expected, msg);
+    };
+
+    it('should remove a dashed line', () => {
+      let input = [
+        '    1. Arsenal         38    26   9   3    79  -  36    87',
+        '   -------------------------------------------------------',
+      ];
+      let expected = ['    1. Arsenal         38    26   9   3    79  -  36    87'];
+      testExpectedResult(input, expected);
+
+      input = [
+        '   -------------------------------------------------------',
+        '    2. Liverpool       38    24   8   6    67  -  30    80',
+      ];
+      expected = ['    2. Liverpool       38    24   8   6    67  -  30    80'];
+      testExpectedResult(input, expected);
+    });
+
+    it('should remove empty rows', () => {
+      const input = [
+        '    1. Arsenal         38    26   9   3    79  -  36    87',
+        '                                                          ',
+      ];
+      const expected = ['    1. Arsenal         38    26   9   3    79  -  36    87'];
+      testExpectedResult(input, expected);
+    });
+  });
+
+  describe('parseRows()', () => {
+    it('should be a function', () => expect(parser.parseRows).to.be.a('function'));
+
+    it('should return an array of rows from data', () => {
+      let input =
+        '       Team            P     W    L   D    F      A     Pts\n' +
+        '    1. Arsenal         38    26   9   3    79  -  36    87\n' +
+        '    2. Liverpool       38    24   8   6    67  -  30    80\n';
+      let expected = [
+        '       Team            P     W    L   D    F      A     Pts',
+        '    1. Arsenal         38    26   9   3    79  -  36    87',
+        '    2. Liverpool       38    24   8   6    67  -  30    80',
+      ];
+      let actual = parser.parseRows(input);
+      expect(actual).to.eql(expected);
+
+      input =
+        '  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn\n' +
+        '   1  88    59    74          53.8       0.00\n' +
+        '   2  79    63    71          46.5       0.00\n';
+      expected = [
+        '  Dy MxT   MnT   AvT   HDDay  AvDP 1HrP TPcpn',
+        '   1  88    59    74          53.8       0.00',
+        '   2  79    63    71          46.5       0.00',
+      ];
+      actual = parser.parseRows(input);
+      expect(actual).to.eql(expected);
+    });
+
+    it('should not include rows with white space or line breaks', () => {
+      const input =
+        '       Team            P     W    L   D    F      A     Pts\n' +
+        '    1. Arsenal         38    26   9   3    79  -  36    87\n' +
+        '   -------------------------------------------------------\n' +
+        '    2. Liverpool       38    24   8   6    67  -  30    80\n';
+      const expected = [
+        '       Team            P     W    L   D    F      A     Pts',
+        '    1. Arsenal         38    26   9   3    79  -  36    87',
+        '    2. Liverpool       38    24   8   6    67  -  30    80',
+      ];
+      const actual = parser.parseRows(input);
+      expect(actual).to.eql(expected);
+    });
+  });
 });
