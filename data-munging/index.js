47d57dd676e21f81c3618f84b054834218da2f65
John Desrosiers <johndrunner@yahoo.com>
2018-04-28 07:55:59 -0700

    Refactor index.js.

--- data-munging/index.js
+++ data-munging/index.js
@@ -2,20 +2,30 @@ const readFile = require('./lib/readFile.js');
 const weatherParser = require('./lib/weatherParser.js');
 const footballParser = require('./lib/footballParser.js');
 
-function renderWeather() {
-  return readFile('./data/weather.dat', 'utf8')
-    .then((data) => {
-      const result = weatherParser.dayWithLowestTempSpread(data);
-      return console.log(`Day ${result} has the lowest temp spread.`);
-    });
-}
+main();
 
-function renderSoccer() {
-  readFile('./data/football.dat', 'utf8')
-    .then(data => footballParser.teamWithLeastForAgainstDiff(data))
-    .then(team => console.log(`${team} has the least difference of 'for' and 'against' goals.`));
-}
+function main() {
+  renderWeather()
+    .then(renderSoccer)
+    .catch((err) => { throw err; });
 
-renderWeather()
-  .then(renderSoccer)
-  .catch((err) => { throw err; });
+  function renderWeather() {
+    return readData('weather.dat')
+      .then(data => weatherParser.dayWithLowestTempSpread(data))
+      .then(result => displayMsg(`Day ${result} has the lowest temp spread.`));
+  }
+
+  function readData(fname) {
+    return readFile(`./data/${fname}`, 'utf8');
+  }
+
+  function displayMsg(msg) {
+    console.log(msg); // eslint-disable-line
+  }
+
+  function renderSoccer() {
+    readData('football.dat')
+      .then(data => footballParser.teamWithLeastForAgainstDiff(data))
+      .then(team => displayMsg(`${team} has the least difference of 'for' and 'against' goals.`));
+  }
+}
