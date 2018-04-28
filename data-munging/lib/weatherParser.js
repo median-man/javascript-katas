6c02695b9a9f519c571aa0992422dc0f9b8ea63f
John Desrosiers <johndrunner@yahoo.com>
2018-04-28 07:37:53 -0700

    Implement utils.findLeast() in weatherParser.

--- data-munging/lib/weatherParser.js
+++ data-munging/lib/weatherParser.js
@@ -1,4 +1,5 @@
 const parser = require('./parser.js');
+const { findLeast } = require('./utils.js');
 
 module.exports = {
   parser,
@@ -18,16 +19,8 @@ module.exports = {
 
   dayWithLowestTempSpread: function dayWithLowestTempSpread(weatherData) {
     const observations = this.parse(weatherData);
-    const tempSpread = obs => obs.maxTemp - obs.minTemp;
-    const getObsWithLowerSpread = (obs1, obs2) => {
-      const spread1 = tempSpread(obs1);
-      const spread2 = tempSpread(obs2);
-      return (spread1 > spread2) ? obs2 : obs1;
-    };
-    let resultObs = observations[0];
-    observations.forEach((obs) => {
-      resultObs = getObsWithLowerSpread(resultObs, obs);
-    });
-    return resultObs.day;
+    const tempSpread = ({ maxTemp, minTemp }) => maxTemp - minTemp;
+    const index = findLeast(observations, tempSpread);
+    return observations[index].day;
   },
 };
