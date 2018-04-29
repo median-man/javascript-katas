32ba796b92b308fd2912ec7312c22332b7183e0e
John Desrosiers <johndrunner@yahoo.com>
2018-04-29 06:54:38 -0700

    Modify and refactor index.js.
    Add dependency for parser.js.
    Fix implementation of weatherParser.js
    Refactor index.js.

--- data-munging/index.js
+++ data-munging/index.js
@@ -1,31 +1,41 @@
 const readFile = require('./lib/readFile.js');
 const weatherParser = require('./lib/weatherParser.js');
 const footballParser = require('./lib/footballParser.js');
+const parser = require('./lib/parser.js');
 
 main();
 
 function main() {
-  renderWeather()
-    .then(renderSoccer)
-    .catch((err) => { throw err; });
+  const weatherPipe = pipe(
+    parser.parseRows.bind(parser),
+    weatherParser.dayWithLowestTempSpread.bind(weatherParser),
+    renderWeather,
+  );
+  readData('weather.dat').then(weatherPipe);
 
-  function renderWeather() {
-    return readData('weather.dat')
-      .then(data => weatherParser.dayWithLowestTempSpread(data))
-      .then(result => displayMsg(`Day ${result} has the lowest temp spread.`));
+  const soccerPipe = pipe(
+    footballParser.teamWithLeastForAgainstDiff.bind(footballParser),
+    renderSoccer,
+  );
+  readData('football.dat').then(soccerPipe);
+
+  function pipe(...fns) {
+    return x => fns.reduce((y, fn) => fn(y), x);
   }
 
   function readData(fname) {
     return readFile(`./data/${fname}`, 'utf8');
   }
 
+  function renderWeather(day) {
+    displayMsg(`Day ${day} has the lowest temp spread.`);
+  }
+
   function displayMsg(msg) {
     console.log(msg); // eslint-disable-line
   }
 
-  function renderSoccer() {
-    readData('football.dat')
-      .then(data => footballParser.teamWithLeastForAgainstDiff(data))
-      .then(team => displayMsg(`${team} has the least difference of 'for' and 'against' goals.`));
+  function renderSoccer(team) {
+    displayMsg(`${team} has the least difference of 'for' and 'against' goals.`);
   }
 }
