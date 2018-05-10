099a3b07a55c27388e8bd54bf3de6279bf1d7b03
John Desrosiers <johndrunner@yahoo.com>
2018-05-10 08:11:01 -0700

    Ready to begin refactor for factory methods.

--- video-store/statement.js
+++ video-store/statement.js
@@ -1,28 +1,54 @@
 /* eslint-disable */
-const Customer = require('./Customer');
+function statement(customer, movies) {
+    let result = `Rental Record for ${customer.name}\n`;
+    for (const r of customer.rentals) {
+      result += `\t${movieFor(r).title}\t${amountFor(r)}\n`;
+    }
+    result += `Amount owed is ${totalAmount()}\n`;
+    result += `You earned ${totalFrequentRenterPoints()} frequent renter points\n`;
+    return result;
 
-function statement(customerData, movies) {
-  const customer = new Customer(customerData, movies);
-  let result = `Rental Record for ${customer.name}\n`;
-  for (const r of customer.rentals) {
-    result += `\t${r.movie.title}\t${r.amount}\n`;
+  function movieFor(rental) {
+    return movies[rental.movieID];
   }
-  result += `Amount owed is ${customer.amount}\n`;
-  result += `You earned ${customer.frequentRenterPoints} frequent renter points\n`;
-  return result;
+
+  function totalAmount() {
+    return customer.rentals
+      .reduce((total, rental) => total + amountFor(rental), 0);
+  }
+
+  function totalFrequentRenterPoints() {
+    return customer.rentals
+      .map(rental => frequentRenterPointsFor(rental))
+      .reduce((total, points) => total + points, 0);
+  }
+
+  function amountFor(rental) {
+    const lateFee = 1.5;
+    const { code } = movieFor(rental);
+    if (code === 'new') {
+      return rental.days * 3;
+    }
+    if (code === 'childrens') {
+      return 1.5 + overdueAmount(rental.days, 3);
+    }
+    return 2 + overdueAmount(rental.days, 2);
+
+    function overdueAmount(days, maxDays) {
+      const isOverdue = days > maxDays;
+      if (isOverdue) {
+        return (days - maxDays) * lateFee;
+      }
+      return 0;
+    }
+  }
+
+  function frequentRenterPointsFor(r) {
+    const newReleaseBonusDays = 3;
+    const isNewMovie = movieFor(r).code === 'new';
+    const qualifiesForNewReleaseBonus = isNewMovie && r.days >= newReleaseBonusDays;
+    return qualifiesForNewReleaseBonus ? 2 : 1;
+  }
 }
 
-function htmlStatement(customerData, movies) {
-  const customer = new Customer(customerData, movies);
-  let result = `<h1>Rental Record for <em>${customer.name}</em></h1>\n` +
-    '<table>\n';
-  for (const r of customer.rentals) {
-    result += `<tr><td>${r.movie.title}</td><td>${r.amount}</td></tr>\n`;
-  }
-  result += '</table>\n' +
-    `<p>Amount owed is <em>${customer.amount}</em></p>\n` +
-    `<p>You earned <em>${customer.frequentRenterPoints}</em> frequent renter points</p>\n`;
-  return result;
-}
-
-module.exports = { statement, htmlStatement };
+module.exports = { statement };
