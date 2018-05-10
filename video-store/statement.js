a49bc0dc76860ba6eac5be44745f9714ffa27106
John Desrosiers <johndrunner@yahoo.com>
2018-05-10 13:22:07 -0700

    Refactor using factory methods for customer and rental objects.

--- video-store/statement.js
+++ video-store/statement.js
@@ -1,38 +1,61 @@
 /* eslint-disable */
-function statement(customer, movies) {
-    let result = `Rental Record for ${customer.name}\n`;
-    for (const r of customer.rentals) {
-      result += `\t${movieFor(r).title}\t${amountFor(r)}\n`;
-    }
-    result += `Amount owed is ${totalAmount()}\n`;
-    result += `You earned ${totalFrequentRenterPoints()} frequent renter points\n`;
-    return result;
+function statement(customerData, movies) {
+  const customer = createCustomer(customerData, movies);
+  let result = `Rental Record for ${customer.name()}\n`;
+  for (const r of customer.rentals()) {
+    result += `\t${r.movie().title}\t${r.amount()}\n`;
+  }
+  result += `Amount owed is ${customer.amount()}\n`;
+  result += `You earned ${customer.frequentRenterPoints()} frequent renter points\n`;
+  return result;
+}
 
-  function movieFor(rental) {
-    return movies[rental.movieID];
+function createCustomer(data, movies) {
+  return {
+    name: () => data.name,
+    rentals,
+    amount,
+    frequentRenterPoints,
   }
 
-  function totalAmount() {
-    return customer.rentals
-      .reduce((total, rental) => total + amountFor(rental), 0);
+  function rentals() {
+    return data.rentals.map(rentalData => createRental(rentalData, movies));
   }
 
-  function totalFrequentRenterPoints() {
-    return customer.rentals
-      .map(rental => frequentRenterPointsFor(rental))
-      .reduce((total, points) => total + points, 0);
+  function amount() {
+    return rentals()
+      .reduce((total, rental) => total + rental.amount(), 0);
   }
 
-  function amountFor(rental) {
+  function frequentRenterPoints() {
+    return rentals()
+      .reduce((total, rental) => total + rental.frequentRenterPoints(), 0);
+  }
+}
+
+function createRental(data, movies) {
+  return {
+    days: () => data.days,
+    movieID: () => data.movieID,
+    movie,
+    amount,
+    frequentRenterPoints,
+  };
+
+  function movie() {
+    return movies[data.movieID];
+  }
+
+  function amount() {
     const lateFee = 1.5;
-    const { code } = movieFor(rental);
+    const { code } = movie();
     if (code === 'new') {
-      return rental.days * 3;
+      return data.days * 3;
     }
     if (code === 'childrens') {
-      return 1.5 + overdueAmount(rental.days, 3);
+      return 1.5 + overdueAmount(data.days, 3);
     }
-    return 2 + overdueAmount(rental.days, 2);
+    return 2 + overdueAmount(data.days, 2);
 
     function overdueAmount(days, maxDays) {
       const isOverdue = days > maxDays;
@@ -43,10 +66,10 @@ function statement(customer, movies) {
     }
   }
 
-  function frequentRenterPointsFor(r) {
+  function frequentRenterPoints() {
     const newReleaseBonusDays = 3;
-    const isNewMovie = movieFor(r).code === 'new';
-    const qualifiesForNewReleaseBonus = isNewMovie && r.days >= newReleaseBonusDays;
+    const isNewMovie = movie().code === 'new';
+    const qualifiesForNewReleaseBonus = isNewMovie && data.days >= newReleaseBonusDays;
     return qualifiesForNewReleaseBonus ? 2 : 1;
   }
 }
