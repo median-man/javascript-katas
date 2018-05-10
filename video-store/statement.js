6c14bd4eaefe56e06f6adbc519318716926202e7
John Desrosiers <johndrunner@yahoo.com>
2018-05-10 07:10:34 -0700

    Add htmlStatement() using es6 classes.

--- video-store/statement.js
+++ video-store/statement.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 const Customer = require('./Customer');
+
 function statement(customerData, movies) {
   const customer = new Customer(customerData, movies);
   let result = `Rental Record for ${customer.name}\n`;
@@ -11,4 +12,17 @@ function statement(customerData, movies) {
   return result;
 }
 
-module.exports = { statement };
+function htmlStatement(customerData, movies) {
+  const customer = new Customer(customerData, movies);
+  let result = `<h1>Rental Record for <em>${customer.name}</em></h1>\n` +
+    '<table>\n';
+  for (const r of customer.rentals) {
+    result += `<tr><td>${r.movie.title}</td><td>${r.amount}</td></tr>\n`;
+  }
+  result += '</table>\n' +
+    `<p>Amount owed is <em>${customer.amount}</em></p>\n` +
+    `<p>You earned <em>${customer.frequentRenterPoints}</em> frequent renter points</p>\n`;
+  return result;
+}
+
+module.exports = { statement, htmlStatement };
