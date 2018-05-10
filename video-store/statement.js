67bb769c2d661b291ae3cf9f027efd81f6cbfab2
John Desrosiers <johndrunner@yahoo.com>
2018-05-10 14:03:42 -0700

    Ready to refactor for data transformation.

--- video-store/statement.js
+++ video-store/statement.js
@@ -1,75 +1,38 @@
 /* eslint-disable */
-function statement(customerData, movies) {
-  const customer = createCustomer(customerData, movies);
-  let result = `Rental Record for ${customer.name()}\n`;
-  for (const r of customer.rentals()) {
-    result += `\t${r.movie().title}\t${r.amount()}\n`;
-  }
-  result += `Amount owed is ${customer.amount()}\n`;
-  result += `You earned ${customer.frequentRenterPoints()} frequent renter points\n`;
-  return result;
-}
-
-function htmlStatement(customerData, movies) {
-  const customer = createCustomer(customerData, movies);
-  let result = `<h1>Rental Record for <em>${customer.name()}</em></h1>\n`;
-  result += '<table>\n';
-    for (const r of customer.rentals()) {
-      result += `<tr><td>${r.movie().title}</td><td>${r.amount()}</td></tr>\n`;
+function statement(customer, movies) {
+    let result = `Rental Record for ${customer.name}\n`;
+    for (const r of customer.rentals) {
+      result += `\t${movieFor(r).title}\t${amountFor(r)}\n`;
     }
-  result += '</table>\n';
-  result += `<p>Amount owed is <em>${customer.amount()}</em></p>\n`;
-  result += `<p>You earned <em>${customer.frequentRenterPoints()}</em> frequent renter points</p>\n`;
-    
-  return result;
-}
+    result += `Amount owed is ${totalAmount()}\n`;
+    result += `You earned ${totalFrequentRenterPoints()} frequent renter points\n`;
+    return result;
 
-function createCustomer(data, movies) {
-  return {
-    name: () => data.name,
-    rentals,
-    amount,
-    frequentRenterPoints,
+  function movieFor(rental) {
+    return movies[rental.movieID];
   }
 
-  function rentals() {
-    return data.rentals.map(rentalData => createRental(rentalData, movies));
+  function totalAmount() {
+    return customer.rentals
+      .reduce((total, rental) => total + amountFor(rental), 0);
   }
 
-  function amount() {
-    return rentals()
-      .reduce((total, rental) => total + rental.amount(), 0);
+  function totalFrequentRenterPoints() {
+    return customer.rentals
+      .map(rental => frequentRenterPointsFor(rental))
+      .reduce((total, points) => total + points, 0);
   }
 
-  function frequentRenterPoints() {
-    return rentals()
-      .reduce((total, rental) => total + rental.frequentRenterPoints(), 0);
-  }
-}
-
-function createRental(data, movies) {
-  return {
-    days: () => data.days,
-    movieID: () => data.movieID,
-    movie,
-    amount,
-    frequentRenterPoints,
-  };
-
-  function movie() {
-    return movies[data.movieID];
-  }
-
-  function amount() {
+  function amountFor(rental) {
     const lateFee = 1.5;
-    const { code } = movie();
+    const { code } = movieFor(rental);
     if (code === 'new') {
-      return data.days * 3;
+      return rental.days * 3;
     }
     if (code === 'childrens') {
-      return 1.5 + overdueAmount(data.days, 3);
+      return 1.5 + overdueAmount(rental.days, 3);
     }
-    return 2 + overdueAmount(data.days, 2);
+    return 2 + overdueAmount(rental.days, 2);
 
     function overdueAmount(days, maxDays) {
       const isOverdue = days > maxDays;
@@ -80,12 +43,12 @@ function createRental(data, movies) {
     }
   }
 
-  function frequentRenterPoints() {
+  function frequentRenterPointsFor(r) {
     const newReleaseBonusDays = 3;
-    const isNewMovie = movie().code === 'new';
-    const qualifiesForNewReleaseBonus = isNewMovie && data.days >= newReleaseBonusDays;
+    const isNewMovie = movieFor(r).code === 'new';
+    const qualifiesForNewReleaseBonus = isNewMovie && r.days >= newReleaseBonusDays;
     return qualifiesForNewReleaseBonus ? 2 : 1;
   }
 }
 
-module.exports = { statement, htmlStatement };
+module.exports = { statement };
--- video-store/test.js
+++ video-store/test.js
@@ -1,53 +1,54 @@
 const { expect } = require('chai');
-const { statement, htmlStatement } = require('./statement');
+const { statement } = require('./statement');
+
+const testCases = [
+  {
+    id: '0',
+    customer: {
+      name: 'john',
+      rentals: [
+        { movieID: 'F001', days: 3 },
+        { movieID: 'F002', days: 1 },
+      ],
+    },
+    movies: {
+      F001: { title: 'Ran', code: 'regular' },
+      F002: { title: 'Trois Couleurs: Bleu', code: 'regular' },
+    },
+  },
+  {
+    id: '1',
+    customer: {
+      name: 'mary',
+      rentals: [
+        { movieID: 'F001', days: 1 },
+        { movieID: 'F002', days: 4 },
+      ],
+    },
+    movies: {
+      F001: { title: 'Play', code: 'childrens' },
+      F002: { title: 'Dude', code: 'childrens' },
+    },
+  },
+  {
+    id: '2',
+    customer: {
+      name: 'mary',
+      rentals: [
+        { movieID: 'F001', days: 1 },
+      ],
+    },
+    movies: {
+      F001: { title: 'Play', code: 'new' },
+    },
+  },
+];
 
 describe('statement()', () => {
   function testTextStatement(customer, movies, expected) {
     const actual = statement(customer, movies);
     expect(actual).to.equal(expected);
   }
-  const testCases = [
-    {
-      id: '0',
-      customer: {
-        name: 'john',
-        rentals: [
-          { movieID: 'F001', days: 3 },
-          { movieID: 'F002', days: 1 },
-        ],
-      },
-      movies: {
-        F001: { title: 'Ran', code: 'regular' },
-        F002: { title: 'Trois Couleurs: Bleu', code: 'regular' },
-      },
-    },
-    {
-      id: '1',
-      customer: {
-        name: 'mary',
-        rentals: [
-          { movieID: 'F001', days: 1 },
-          { movieID: 'F002', days: 4 },
-        ],
-      },
-      movies: {
-        F001: { title: 'Play', code: 'childrens' },
-        F002: { title: 'Dude', code: 'childrens' },
-      },
-    },
-    {
-      id: '2',
-      customer: {
-        name: 'mary',
-        rentals: [
-          { movieID: 'F001', days: 1 },
-        ],
-      },
-      movies: {
-        F001: { title: 'Play', code: 'new' },
-      },
-    },
-  ];
 
   describe('when two regular movies are rented with one overdue', () => {
     it('should return text statement for test case 0', () => {
@@ -96,20 +97,20 @@ describe('statement()', () => {
       expect(shouldThrow).to.throw(expectedMsg);
     });
   });
+});
 
-  describe('when format is html', () => {
-    it('should return html rental statement for case 0', () => {
-      const expected =
-        '<h1>Rental Record for <em>john</em></h1>\n' +
-        '<table>\n' +
-        '<tr><td>Ran</td><td>3.5</td></tr>\n' +
-        '<tr><td>Trois Couleurs: Bleu</td><td>2</td></tr>\n' +
-        '</table>\n' +
-        '<p>Amount owed is <em>5.5</em></p>\n' +
-        '<p>You earned <em>2</em> frequent renter points</p>\n';
-      const { customer, movies } = testCases[0];
-      const format = 'html';
-      expect(htmlStatement(customer, movies, format)).to.equal(expected);
-    });
+describe.skip('htmlStatment()', () => {
+  it('should return html rental statement for case 0', () => {
+    const expected =
+      '<h1>Rental Record for <em>john</em></h1>\n' +
+      '<table>\n' +
+      '<tr><td>Ran</td><td>3.5</td></tr>\n' +
+      '<tr><td>Trois Couleurs: Bleu</td><td>2</td></tr>\n' +
+      '</table>\n' +
+      '<p>Amount owed is <em>5.5</em></p>\n' +
+      '<p>You earned <em>2</em> frequent renter points</p>\n';
+    const { customer, movies } = testCases[0];
+    const format = 'html';
+    expect(htmlStatement(customer, movies, format)).to.equal(expected);
   });
 });
