559ffabf72f0ad42436c2604c819e51a708252c1
John Desrosiers <johndrunner@yahoo.com>
2018-05-11 06:37:34 -0700

    Create createStatementData.js.
    Create createStatementData.js which exports the createStatementData
    method previously contained in statement.js.
    
    Refactor statment.js to import createStatementData().

--- video-store/statement.js
+++ video-store/statement.js
@@ -1,3 +1,5 @@
+const createStatementData = require('./createStatementData');
+
 module.exports.statement = function statement(customer, movies) {
   const data = createStatementData(customer, movies);
   let result = `Rental Record for ${data.name}\n`;
@@ -29,60 +31,3 @@ module.exports.htmlStatement = function htmlStatement(customer, movies) {
       .join('');
   }
 };
-
-function createStatementData(customer, movies) {
-  const thisData = Object.assign({}, customer);
-  thisData.rentals = customer.rentals.map(rentalData => createRental(rentalData));
-  thisData.totalAmount = totalAmount();
-  thisData.totalFrequentRenterPoints = totalFrequentRenterPoints();
-  return thisData;
-
-  function createRental(rentalData) {
-    const thisRental = Object.assign({}, rentalData);
-    thisRental.title = movieFor(rentalData).title;
-    thisRental.amount = amountFor(thisRental);
-    return thisRental;
-  }
-
-  function movieFor(rental) {
-    return movies[rental.movieID];
-  }
-
-  function totalAmount() {
-    return customer.rentals
-      .reduce((total, rental) => total + amountFor(rental), 0);
-  }
-
-  function totalFrequentRenterPoints() {
-    return customer.rentals
-      .map(rental => frequentRenterPointsFor(rental))
-      .reduce((total, points) => total + points, 0);
-  }
-
-  function amountFor(rental) {
-    const lateFee = 1.5;
-    const { code } = movieFor(rental);
-    if (code === 'new') {
-      return rental.days * 3;
-    }
-    if (code === 'childrens') {
-      return 1.5 + overdueAmount(rental.days, 3);
-    }
-    return 2 + overdueAmount(rental.days, 2);
-
-    function overdueAmount(days, maxDays) {
-      const isOverdue = days > maxDays;
-      if (isOverdue) {
-        return (days - maxDays) * lateFee;
-      }
-      return 0;
-    }
-  }
-
-  function frequentRenterPointsFor(r) {
-    const newReleaseBonusDays = 3;
-    const isNewMovie = movieFor(r).code === 'new';
-    const qualifiesForNewReleaseBonus = isNewMovie && r.days >= newReleaseBonusDays;
-    return qualifiesForNewReleaseBonus ? 2 : 1;
-  }
-}
