bf545def87e3313a27b1363fef6a2444ef5071b8
John Desrosiers <johndrunner@yahoo.com>
2018-05-16 06:05:41 -0700

    Complete primeFactors().

---
+++ prime-factors/primeFactors.js
@@ -0,0 +1,10 @@
+module.exports = function primeFactors(n) {
+  const factors = [];
+  let remainder = n;
+  for (let divisor = 2; remainder > 1; divisor += 1) {
+    for (; remainder % divisor === 0; remainder /= divisor) {
+      factors.push(divisor);
+    }
+  }
+  return factors;
+};
---
+++ prime-factors/test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const primeFactors = require('./primeFactors');
+
+describe('primeFactors()', () => {
+  const testPrimeFactors = (input, expected) => {
+    const msg = `should return [${expected.join()}] when n = ${input}`;
+    const expectation = () => expect(primeFactors(input)).to.deep.equal(expected);
+    it(msg, expectation);
+  };
+
+  testPrimeFactors(1, []);
+  testPrimeFactors(2, [2]);
+  testPrimeFactors(3, [3]);
+  testPrimeFactors(4, [2, 2]);
+  testPrimeFactors(5, [5]);
+  testPrimeFactors(6, [2, 3]);
+  testPrimeFactors(8, [2, 2, 2]);
+  testPrimeFactors(9, [3, 3]);
+  testPrimeFactors(2 * 2 * 3 * 3 * 5 * 7 * 13, [2, 2, 3, 3, 5, 7, 13]);
+});
