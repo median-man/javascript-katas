7297fde42029cb7733db2a77d3c3407f4121fae0
John Desrosiers <john.desro@gmail.com>
2018-11-18 09:55:00 -0800

    Complete greeter kata
    This commit completes the greeter kata with 12 passing tests. Call
    `createGreeter(date, logger)` where date is a Date instance and logger
    implements a log method which accepts one string parameter.
    
    - Adds createGreeter to greeter.js with greet method.
    - Adds 12 passing tests for greeter.

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -1,1 +1,24 @@
+function createGreeter (date, logger) {
+  function greet (name) {
+    const message = `${greeting()}, ${formatName(name)}`
+    logger.log(message)
+    return message
+  }
 
+  function greeting () {
+    const hours = date.getHours()
+    if (hours < 6 || hours >= 22) return 'Good night'
+    if (hours < 12) return 'Good morning'
+    if (hours < 18) return 'Hello'
+    return 'Good evening'
+  }
+
+  function formatName (name) {
+    const trimmed = name.trim()
+    return trimmed.charAt(0).toUpperCase() + trimmed.slice(1)
+  }
+
+  return { greet }
+}
+
+module.exports = { createGreeter }
--- greeter/greeter.test.js
+++ greeter/greeter.test.js
@@ -2,5 +2,71 @@ const should = require('chai').should()
 const { createGreeter } = require('./greeter')
 
 describe('createGreeter', () => {
-  it('should return an object', () => should.exist(createGreeter))
+  it('should exist', () => should.exist(createGreeter))
+
+  describe('greeter.greet', () => {
+    let greeter
+    let logger
+
+    beforeEach(() => {
+      logger = { log: () => {} }
+      setGreeterHour(12)
+    })
+
+    function setGreeterHour (hour) {
+      const date = new Date()
+      date.setHours(hour)
+      greeter = createGreeter(date, logger)
+    }
+
+    it('should exist', () => should.exist(createGreeter().greet))
+
+    it('should return "Hello, John"', () => {
+      greeter.greet('John').should.equal('Hello, John')
+    })
+
+    it('should return "Hello, Kate"', () => {
+      greeter.greet('Kate').should.equal('Hello, Kate')
+    })
+
+    it('should trim whitespace', () => {
+      greeter.greet(' Kate ').should.equal('Hello, Kate')
+    })
+
+    it('should capitalize the first letter of name', () => {
+      greeter.greet('kate').should.equal('Hello, Kate')
+    })
+
+    it('should return "Good morning, Kate" at 6:00 AM', () => {
+      setGreeterHour(6)
+      greeter.greet('kate').should.equal('Good morning, Kate')
+    })
+
+    it('should return "Good morning, Kate" at 11:00 AM', () => {
+      setGreeterHour(11)
+      greeter.greet('kate').should.equal('Good morning, Kate')
+    })
+
+    it('should return "Good evening, Kate" at 6:00 PM', () => {
+      setGreeterHour(18)
+      greeter.greet('kate').should.equal('Good evening, Kate')
+    })
+
+    it('should return "Good night, Kate" at 10:00 PM', () => {
+      setGreeterHour(22)
+      greeter.greet('kate').should.equal('Good night, Kate')
+    })
+
+    it('should return "Good night, Kate" at 05:00 AM', () => {
+      setGreeterHour(5)
+      greeter.greet('kate').should.equal('Good night, Kate')
+    })
+
+    it('should call logger.log with greeting', () => {
+      let logCalledWith = []
+      logger.log = (...args) => { logCalledWith = args }
+      greeter.greet('kate')
+      logCalledWith[0].should.equal('Hello, Kate')
+    })
+  })
 })
