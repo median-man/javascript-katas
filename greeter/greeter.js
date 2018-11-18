1ec8b670238a051db989241a2e3ba55a18ec54e1
John Desrosiers <john.desro@gmail.com>
2018-11-18 08:21:54 -0800

    Complete greeter kata
    This commit adds the complete greeter kata. Use createGreeter(date, logger)
    to create a new greeter instance. Call greeter.greet(name) to log a
    greeting by passing the greeting to logger.log. Returns the greeting.
    
    The `date` parameter is optional. Defaults to current date and time.
    Should be a Date instance.
    
    The `logger` param. is option. Defaults to built-in console. Should
    implement `log` method.
    
    - Adds 12 passing unit tests for greeter.

--- greeter/greeter.js
+++ greeter/greeter.js
@@ -1,1 +1,22 @@
-module.exports.greeter = function greeter () {}
+function createGreeter (date = new Date(), logger = console) {
+  const hours = date.getHours()
+
+  const greeting = () => {
+    if (hours >= 22 || hours < 6) return 'Good night'
+    if (hours >= 18) return 'Good evening'
+    if (hours >= 12) return 'Hello'
+    return 'Good morning'
+  }
+
+  function greet (name) {
+    const trimmedName = name.trim()
+    const capitalizedName = trimmedName.charAt(0).toUpperCase() + trimmedName.slice(1)
+    const message = `${greeting()}, ${capitalizedName}`
+    logger.log(message)
+    return message
+  }
+
+  return { greet }
+}
+
+module.exports = { createGreeter }
--- greeter/greeter.test.js
+++ greeter/greeter.test.js
@@ -1,6 +1,87 @@
 const should = require('chai').should()
-const { greeter } = require('./greeter')
+const { createGreeter } = require('./greeter')
 
-describe('greeter', () => {
-  it('should exist', () => should.exist(greeter))
+const quietLogger = {
+  log: () => {}
+}
+
+describe('createGreeter', () => {
+  it('should return an object', () => createGreeter().should.be.an('object'))
+})
+
+describe('greeter.greet', () => {
+
+  it('should exist', () => {
+    should.exist(createGreeter(new Date(), quietLogger).greet)
+  })
+
+  it('should say good morning when the time is 6:00 AM', () => {
+    const date = new Date('2018-01-01 06:00')
+    createGreeter(date, quietLogger)
+      .greet('kate')
+      .should.equal('Good morning, Kate')
+  })
+
+  it('should say good morning when the time is 11:59', () => {
+    const date = new Date('2018-01-01 11:59')
+    createGreeter(date, quietLogger)
+      .greet('kate')
+      .should.equal('Good morning, Kate')
+  })
+
+  it('should say good evening when the time is 18:00', () => {
+    const date = new Date('2018-01-01 18:00')
+    createGreeter(date, quietLogger)
+      .greet('kate')
+      .should.equal('Good evening, Kate')
+  })
+
+  it('should say good night when the time is 22:00', () => {
+    const date = new Date('2018-01-01 22:00')
+    createGreeter(date, quietLogger)
+      .greet('kate')
+      .should.equal('Good night, Kate')
+  })
+
+  it('should say good night when the time is 00:00', () => {
+    const date = new Date('2018-01-01 01:00')
+    createGreeter(date, quietLogger)
+      .greet('kate')
+      .should.equal('Good night, Kate')
+  })
+
+  it('should log to console when called', () => {
+    const date = new Date('2018-01-01 12:00')
+    let logCalledWith = []
+    const logger = {
+      log: (...args) => {
+        logCalledWith = args
+      }
+    }
+
+    createGreeter(date, logger).greet('Kate')
+
+    logCalledWith.should.have.length.greaterThan(0, 'logger.log was not called')
+    logCalledWith[0].should.equal('Hello, Kate')
+  })
+
+  describe('when time is "12:01"', () => {
+    let greeter
+
+    beforeEach(() => {
+      greeter = createGreeter(new Date('2018-01-01 12:00'), quietLogger)
+    })
+
+    it('should return "Hello, John"', () =>
+      greeter.greet('John').should.equal('Hello, John'))
+
+    it('should return "Hello, Kate"', () =>
+      greeter.greet('Kate').should.equal('Hello, Kate'))
+
+    it('should trim white space', () =>
+      greeter.greet(' Kate ').should.equal('Hello, Kate'))
+
+    it('should capitalize the first letter of the name', () =>
+      greeter.greet(' kate ').should.equal('Hello, Kate'))
+  })
 })
