12b9d0eaf380cdf0fe4050bac8e40b285fc2ac28
John Desrosiers <john.desro@gmail.com>
2018-11-25 05:00:42 -0800

    Solve new string-calculator
    Delete all code from string-calculator and test.
    Solve string-calculator with new specs from osherove.com.
    
    For the first time, I was inspired to incorporate a pipe in my solution.
    The pipe makes the flow easy to read in the add method.

--- string-calculator/string-calculator.js
+++ string-calculator/string-calculator.js
@@ -1,47 +1,66 @@
-function add (s) {
-  const MAX_NUM = 1000
-  return split(s)
-    .map(toValidNumber)
-    .filter(n => n <= MAX_NUM)
-    .reduce(sum2, 0)
-}
+function add (str) {
+  if (!str) return 0
 
-function split (s) {
-  const hasCustomDelimiters = s.slice(0, 2) === '//'
-  if (hasCustomDelimiters) return toCSV(s).split(',')
-  return s.split(/[,\s]/g)
-}
-
-// Returns string with custom delimiters defined in the first row
-// replaced with commas in the body. The body is everything after
-// the first row. Example first row: "//[--][=]\n"
-// E.g. "//[-]\n1-1" returns "1,1"
-function toCSV (s) {
-  const endOfFirstLine = s.indexOf('\n')
-  const firstLine = s.slice(endOfFirstLine)
-  const body = s.slice(endOfFirstLine + 1)
-  const delimiters = firstLine
-    .slice(2, endOfFirstLine)
-    .split('[')
-    .map(removeLastCharacter)
-  return delimiters.reduce(
-    (input, delimiter) => input.replace(delimiter, ','),
-    body
+  return pipe(
+    str,
+    getCSV,
+    parseCsv,
+    validate, // throws on invalid nums
+    rangeFilter,
+    sumArr
   )
 }
 
-function removeLastCharacter (s) {
-  return s.slice(0, s.length - 1)
+function pipe (input, ...funcs) {
+  return funcs.reduce((acc, func) => func(acc), input)
 }
 
-function toValidNumber (s) {
-  const n = Number.parseFloat(s)
-  if (n < 0) throw new Error(`Negative numbers not allowed: ${n}.`)
-  return n || 0
+function getCSV (str) {
+  const noCustomDelimiter = str.charAt(0) !== '/'
+  if (noCustomDelimiter) return str
+
+  const delimStart = 2 // first char after "//"
+  const endOfFirstLine = str.indexOf('\n')
+  let delimiters = [str.charAt(delimStart)]
+
+  const hasBrackets = str.charAt(delimStart) === '['
+  if (hasBrackets) {
+    delimiters = str.slice(delimStart + 1, endOfFirstLine - 1).split('][')
+  }
+
+  const values = str.slice(endOfFirstLine + 1)
+  return regExps(delimiters).reduce((acc, re) => acc.replace(re, ','), values)
 }
 
-function sum2 (x, y) {
-  return x + y
+// escapes regexp special characters (e.g. "$" gets escaped)
+function regExps (strings) {
+  return strings.map(escapeRegExp).map(delim => new RegExp(delim, 'g'))
+}
+
+function escapeRegExp (text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&')
+}
+
+function parseCsv (csv) {
+  return getCSV(csv)
+    .split(/[,\n]/)
+    .map(Number.parseFloat)
+}
+
+function validate (numbers) {
+  const negatives = numbers.filter(num => num < 0)
+  if (negatives.length) {
+    throw new Error(`Negative numbers not allowed: ${negatives.join(',')}`)
+  }
+  return numbers
+}
+
+function rangeFilter (numbers) {
+  return numbers.filter(num => num <= 1000)
+}
+
+function sumArr (numbers) {
+  return numbers.reduce((sum, num) => sum + num, 0)
 }
 
 module.exports = { add }
--- string-calculator/test.js
+++ string-calculator/test.js
@@ -1,44 +1,73 @@
 const should = require('chai').should()
 const { add } = require('./string-calculator')
 
-describe('string-calculator', () => {
-  it('should exist', () => should.exist(add))
+describe('string-calculator.add', () => {
+  describe('basic cases', () => {
+    it('should exist', () => {
+      should.exist(add)
+    })
 
-  it('should return 0 when given an empty string', () => {
-    add('').should.equal(0)
+    it('should return 0 when given an empty string', () => {
+      add('').should.equal(0)
+    })
+
+    it('should return sum for a single number', () => {
+      add('1').should.equal(1)
+    })
+
+    it('should return sum for two numbers', () => {
+      add('1,2').should.equal(3)
+    })
+
+    it('should treat new lines as a separator', () => {
+      add('1\n2\n3,4').should.equal(10)
+    })
   })
 
-  it('should return 1', () => {
-    add('1').should.equal(1)
+  describe('custom delimiters', () => {
+    const testDelimiter = delimiter => {
+      it(`should handle custom delimiter: "${delimiter}"`, () => {
+        add(`//${delimiter}\n1${delimiter}2${delimiter}3`).should.equal(6)
+      })
+    }
+    testDelimiter(',')
+    testDelimiter('#')
+    testDelimiter('+')
   })
 
-  it('should throw with "Negative numbers not allowed: -1.', () => {
-    should.throw(() => add('-1'), 'Negative numbers not allowed: -1.')
+  describe('custom delimiters with brackets', () => {
+    it('should support custom delim. defined with "//[-]"', () => {
+      add('//[-]\n1-2-3').should.equal(6)
+    })
+
+    it('should support custom delim. defined with "//[#-]"', () => {
+      add('//[#-]\n1#-2#-3').should.equal(6)
+    })
+
+    it('should support custom delim. defined with "//[+$]"', () => {
+      add('//[+$]\n1+$2+$3').should.equal(6)
+    })
+
+    it('should support multiple custom delimiters', () => {
+      add('//[-+][$=]\n1$=2-+3').should.equal(6)
+    })
   })
 
-  it('should add two numbers comma separated numbers', () => {
-    add('1,1').should.equal(2)
+  describe('negative numbers', () => {
+    const testNegatives = str => {
+      it(`should throw with "Negative numbers not allowed: ${str}"`, () => {
+        const expectedMessage = `Negative numbers not allowed: ${str}`
+        should.throw(() => add(`${str}`), expectedMessage)
+      })
+    }
+    testNegatives('-1')
+    testNegatives('-2')
+    testNegatives('-3,-4')
   })
 
-  it('should ignore numbers > 1000', () => {
-    add('1,1001').should.equal(1)
-  })
-
-  it('should treat white space as a delimiter', () => {
-    add('1 1').should.equal(2, 'single space')
-    add('1\t1').should.equal(2, 'tab')
-  })
-
-  it('should handle a custom delimiter', () => {
-    let input = '//[-]\n1-1'
-    add(input).should.equal(2, 'delimiter: -')
-
-    input = '//[=]\n1=1'
-    add(input).should.equal(2, 'delimiter: =')
-  })
-
-  it('should support custom delimiter with multiple characters', () => {
-    let input = '//[--]\n1--1'
-    add(input).should.equal(2, 'delimiter: --')
+  describe('range filter', () => {
+    it('should ignore numbers > 1000', () => {
+      add('2,3,1001').should.equal(5)
+    })
   })
 })
