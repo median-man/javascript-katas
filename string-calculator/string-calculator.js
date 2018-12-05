c7ed1602b5b658b0a63e280dda463c9894882076
John Desrosiers <john.desro@gmail.com>
2018-12-05 05:02:40 -0800

    String calculator custom separators and negatives
    - Add method supports custom separators including special RegExp chars.
    - Add method ignored values > 1000.
    - Adds pending test for multi-character separator.

--- string-calculator/string-calculator.js
+++ string-calculator/string-calculator.js
@@ -1,14 +1,33 @@
+RegExp.escape = function (s) {
+  return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+}
+
 const sum = (x, y) => x + y
 const parseCsv = str => str.split(/[,\n]/)
+
+const toCsv = ({ separator, values }) => {
+  const re = new RegExp(RegExp.escape(separator), 'g')
+  return values.replace(re, ',')
+}
 const parseInputStr = s => ({
+  separator: s.charAt(2),
   values: s.slice(s.indexOf('\n') + 1)
 })
 
+const validateNumbers = nums => {
+  const negativeNums = nums.filter(n => n < 0)
+  if (negativeNums.length) {
+    throw new Error(`Negatives not allowed: ${negativeNums.join(',')}`)
+  }
+  return nums
+}
+
 function add (s) {
-  const values = (s[0] === '/') ? parseInputStr(s).values : s
+  const values = s[0] === '/' ? toCsv(parseInputStr(s)) : s
   if (values.length === 0) return 0
-  return parseCsv(values)
-    .map(Number.parseFloat)
+  const numbers = parseCsv(values).map(Number.parseFloat)
+  return validateNumbers(numbers)
+    .filter(num => num <= 1000)
     .reduce(sum)
 }
 
--- string-calculator/test.js
+++ string-calculator/test.js
@@ -6,12 +6,17 @@ describe('string-calculator.add', () => {
     should.exist(add)
   })
 
-  describe('basic csv', () => {
-    const shouldReturnGivenTest = (expected, input) =>
-      it(`should return ${expected} given "${input}"`, () => {
+  const shouldReturnGivenTest = (expected, input, skip = false) => {
+    const fn = skip ? it.skip : it
+    return fn(
+      `should return ${expected} given "${input.replace(/\n/g, '\\n')}"`,
+      () => {
         add(input).should.equal(expected)
-      })
+      }
+    )
+  }
 
+  describe('basic csv', () => {
     shouldReturnGivenTest(0, '')
     shouldReturnGivenTest(1, '1')
     shouldReturnGivenTest(3, '1,2')
@@ -19,12 +24,28 @@ describe('string-calculator.add', () => {
   })
 
   describe('custom separator', () => {
-    it(' "//,\\n1,2,3" should return 6', () => {
-      add('//,\n1,2,3').should.equal(6)
-    })
+    shouldReturnGivenTest(6, '//,\n1,2,3')
+    shouldReturnGivenTest(6, '//#\n1#2#3')
 
-    xit(' "//#\\n1#2#3" should return 6', () => {
-      add('//#\n1#2#3').should.equal(6)
+    // checks a special regexp character for the separator
+    shouldReturnGivenTest(6, '//+\n1+2+3')
+  })
+
+  describe('negative values', () => {
+    it('should throw with "Negatives not allowed: -1"', () => {
+      should.throw(() => add('-1'), 'Negatives not allowed: -1')
     })
+    it('should throw with "Negatives not allowed: -1,-3"', () => {
+      should.throw(() => add('-1,-3'), 'Negatives not allowed: -1,-3')
+    })
+  })
+
+  describe('ignore numbers', () => {
+    shouldReturnGivenTest(1, '//+\n1+1001')
+    shouldReturnGivenTest(1001, '//+\n1+1000')
+  })
+
+  describe('extended separators', () => {
+    shouldReturnGivenTest(6, '//[--]\n1--2--3', true)
   })
 })
