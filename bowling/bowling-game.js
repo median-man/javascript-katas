e413f82e87c0d070c397b959db3fddd1b1b154a2
John Desrosiers <john.desro@gmail.com>
2018-12-11 05:10:18 -0800

    Revert "Bowling game passes one spare test"
    This reverts commit 81bd7c53eeaeb7ed4e710967e34ff441d6d0b27e.
    
    Redo third test of bowling game kata.

--- bowling/bowling-game.js
+++ bowling/bowling-game.js
@@ -1,30 +1,8 @@
 function createBowlingGame () {
-  const rolls = []
-
-  function score () {
-    let score = 0
-    let frameIndex = 0
-
-    for (let frame = 0; frame < 10; frame += 1) {
-      if (isSpare(frameIndex)) {
-        score += 10 + rolls[frameIndex + 2]
-        frameIndex += 2
-      } else {
-        score += rolls[frameIndex] + rolls[frameIndex + 1]
-        frameIndex += 2
-      }
-    }
-    return score
-  }
-
-  function isSpare (frameIndex) {
-    return rolls[frameIndex] + rolls[frameIndex + 1] === 10
-  }
+  let score = 0
   return {
-    roll: pins => {
-      rolls.push(pins)
-    },
-    score
+    roll: (pins) => (score += pins),
+    score: () => score
   }
 }
 
