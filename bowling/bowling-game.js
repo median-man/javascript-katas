28ac6e0c566cb0b27489daf7b2e044de6f0c567b
John Desrosiers <john.desro@gmail.com>
2018-12-12 05:02:04 -0800

    Revert "wip: Add test for one spare and refactor"
    This reverts commit ee1d88f71ef42365e8101b9c348672c2fe697501.
    
    Revert wip on scoring spare to practice the phase from its start.

--- bowling/bowling-game.js
+++ bowling/bowling-game.js
@@ -1,19 +1,8 @@
 function createBowlingGame () {
   let score = 0
-  const rolls = []
-
   return {
-    roll: pins => {
-      rolls.push(pins)
-      return (score += pins)
-    },
-    score: () => {
-      let score = 0
-      for (let i = 0; i < rolls.length; i += 1) {
-        score += rolls[i]
-      }
-      return score
-    }
+    roll: (pins) => (score += pins),
+    score: () => score
   }
 }
 
