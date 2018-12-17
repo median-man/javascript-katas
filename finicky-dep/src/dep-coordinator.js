5e7ecaa30544b4df265eceb7658f846df15320ba
John Desrosiers <john.desro@gmail.com>
2018-12-17 04:08:57 -0800

    Revert "Finicky dependency kata"
    This reverts commit ec3ec9d2dbb3a7a35c7d1ac8fddd2787c4356b02.
    
    My search for a good DI kata continues. This one did not make the cut.

--- finicky-dep/src/dep-coordinator.js
+++
@@ -1,7 +0,0 @@
-const FinickyDependency = require('./dependency')
-
-function getResourceFromDependency (resourceApp = new FinickyDependency()) {
-  return resourceApp.getResource()
-}
-
-module.exports = { getResourceFromDependency }
--- finicky-dep/src/dependency.js
+++
@@ -1,9 +0,0 @@
-class FinickyDependency {
-  getResource () {
-    throw new Error(
-      `You can't do that outside of a production environment. Ah, ah, ahh.`
-    )
-  }
-}
-
-module.exports = FinickyDependency
