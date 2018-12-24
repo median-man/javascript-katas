7a1b41b6c1a01d980bb51395b8389c06b62f17cb
John Desrosiers <john.desro@gmail.com>
2018-12-24 05:55:40 -0800

    Prime factors kata passing test 4

---
+++ prime-factors/primeFactors.js
@@ -0,0 +1,16 @@
+function generate (n) {
+  const primes = []
+
+  if (n > 1) {
+    if (n % 2 === 0) {
+      primes.push(2)
+      n /= 2
+    }
+    if (n > 1) {
+      primes.push(n)
+    }
+  }
+  return primes
+}
+
+module.exports = { generate }
