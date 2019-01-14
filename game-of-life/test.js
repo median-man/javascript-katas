a0415bce320d8b66d2083b760549d07a497c9ffd
John Desrosiers <john.desro@gmail.com>
2019-01-14 05:47:07 -0800

    GOL: add test for all dead cells (red)

--- game-of-life/test.js
+++ game-of-life/test.js
@@ -11,5 +11,28 @@ describe('game-of-life kata', () => {
     it('should return dead cell given "."', () => {
       nextGeneration('.').should.equal('.')
     })
+
+    it('should return all dead cells given all dead cells', () => {
+      const allDead = [
+        '*..',
+        '...',
+        '...'
+      ].join('\n')
+      nextGeneration(allDead).should.equal(allDead)
+    })
+
+    it.skip('should return dead cells for cells with no live neighbors', () => {
+      const previous = [
+        '*..',
+        '...',
+        '...'
+      ].join('\n')
+      const expected = [
+        '...',
+        '...',
+        '...'
+      ].join('\n')
+      nextGeneration(previous).should.equal(expected)
+    })
   })
 })
