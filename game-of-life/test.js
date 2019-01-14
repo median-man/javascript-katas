32c84a97cd8fd1abed386c5e8383860767117e3d
John Desrosiers <john.desro@gmail.com>
2019-01-14 05:39:26 -0800

    GOL: Add nextGeneration test (red)

--- game-of-life/test.js
+++ game-of-life/test.js
@@ -7,5 +7,9 @@ describe('game-of-life kata', () => {
     it('should return empty string given undefined', () => {
       nextGeneration().should.equal('')
     })
+
+    it('should return dead cell given "."', () => {
+      nextGeneration('.').should.equal('.')
+    })
   })
 })
