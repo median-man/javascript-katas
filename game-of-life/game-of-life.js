71594bf525cd2253bb93b5664399f9035b4c9064
John Desrosiers <john.desro@gmail.com>
2019-01-15 05:46:12 -0800

    GOL: refactor to Grid class (green)

--- game-of-life/game-of-life.js
+++ game-of-life/game-of-life.js
@@ -1,7 +1,3 @@
-function getNextCell (previousGen, rowIndex, colIndex) {
-  return '.'
-}
-
 function nextGeneration (previousGen) {
   if (!previousGen) {
     return ''
@@ -9,11 +5,33 @@ function nextGeneration (previousGen) {
   if (previousGen.length === 1) {
     return '.'
   }
-  const nextGrid = previousGen.split('\n').map(row => row.split(''))
-  const previousGrid = previousGen.split('\n').map(row => row.split(''))
+  const nextGrid = new Grid(previousGen)
+  const previousGrid = new Grid(previousGen)
   const nextCellChar = getNextCell(previousGrid, 0, 0)
-  nextGrid[0][0] = nextCellChar
-  return nextGrid.map(row => row.join('')).join('\n')
+  nextGrid.setCellAt(0, 0, nextCellChar)
+  return nextGrid.toString()
+}
+
+class Grid {
+  constructor (str) {
+    this.rows = str.split('\n').map(row => row.split(''))
+  }
+
+  setCellAt (row, col, char) {
+    this.rows[row][col] = char
+  }
+
+  charAt (row, column) {
+    return this.rows[row][column]
+  }
+
+  toString () {
+    return this.rows.map(row => row.join('')).join('\n')
+  }
+}
+
+function getNextCell (previousGen, row, col) {
+  return '.'
 }
 
 module.exports = { nextGeneration }
