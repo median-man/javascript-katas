8a3f54784838aacce14c9d3168e2a1a84efd3d6c
John Desrosiers <john.desro@gmail.com>
2019-01-16 05:25:35 -0800

    Revert "GOL: live cell with two live neighbors lives (green)"
    This reverts commit e6f205587af344eea279b894858b5cc48fc174a3.
    
    Reverts bad test and code to pass it.

--- game-of-life/game-of-life.js
+++ game-of-life/game-of-life.js
@@ -31,13 +31,6 @@ class Grid {
 }
 
 function getNextCell (previousGen, row, col) {
-  const rightCell = previousGen.charAt(0, 1)
-  const downCell = previousGen.charAt(1, 0)
-  const countLiveNeighbors = [rightCell, downCell].filter(char => char === '*')
-    .length
-  if (countLiveNeighbors > 1) {
-    return '*'
-  }
   return '.'
 }
 
