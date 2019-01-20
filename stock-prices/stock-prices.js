b344b4646a79ca0e6bb71eac7a29fe5b44547ff3
John Desrosiers <john.desro@gmail.com>
2019-01-20 08:21:04 -0800

    SP: refactor

--- stock-prices/stock-prices.js
+++ stock-prices/stock-prices.js
@@ -1,11 +1,20 @@
 function buySellPrices (prices) {
-  if (prices.length < 2) {
-    return []
-  }
-  if (prices[0] > prices[1]) {
+  if (doNotBuy(prices)) {
     return []
   }
   return prices
+}
+
+function doNotBuy (prices) {
+  return notEnoughPrices(prices) || noPositiveMarginPossible(prices)
+}
+
+function notEnoughPrices (prices) {
+  return prices.length < 2
+}
+
+function noPositiveMarginPossible (prices) {
+  return prices[0] > prices[1]
 }
 
 module.exports = { buySellPrices }
