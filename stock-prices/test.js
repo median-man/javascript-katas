6d80ff3ff4f6b0a55e090f95c5597d4dea1e3fac
John Desrosiers <john.desro@gmail.com>
2019-01-20 07:19:03 -0800

    SP: should return [] given [] [red]

--- stock-prices/test.js
+++ stock-prices/test.js
@@ -1,11 +1,13 @@
+/* eslint-disable */
+
 require('chai').should()
 
 const { findOptimalBuySellPrices } = require('./stock-prices')
 
 describe('Stock Prices Kata', () => {
   describe('findOptimalBuySellPrices', () => {
-    it('should return null given an empty array', () => {
-      findOptimalBuySellPrices([]).should.be
+    it('should return [] given an empty array', () => {
+      findOptimalBuySellPrices([]).should.eql([])
     })
   })
 })
