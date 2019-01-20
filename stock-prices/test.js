25eca6deb88382802d641d28ad68f2312cf25784
John Desrosiers <john.desro@gmail.com>
2019-01-20 07:11:59 -0800

    SP: just enough to make test 1 fail [red]

--- stock-prices/test.js
+++ stock-prices/test.js
@@ -3,7 +3,7 @@ const { findOptimalBuySellPrices } = require('./stock-prices')
 describe('Stock Prices Kata', () => {
   describe('findOptimalBuySellPrices', () => {
     it('should return null given an empty array', () => {
-      findOptimalBuySellPrices([])
+      findOptimalBuySellPrices([]).should
     })
   })
 })
