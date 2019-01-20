93524d79515ae152a4b978ca51fbe7e3d74c8996
John Desrosiers <john.desro@gmail.com>
2019-01-20 07:09:51 -0800

    SP: initial commit for stock-prices.js [green]

--- stock-prices/test.js
+++ stock-prices/test.js
@@ -1,3 +1,5 @@
+const { findOptimalBuySellPrices } = require('./stock-prices')
+
 describe('Stock Prices Kata', () => {
   describe('findOptimalBuySellPrices', () => {
     it('should return null given an empty array', () => {
