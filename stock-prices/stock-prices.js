9021f0072b6b562974afb657e70268e5519f4e34
John Desrosiers <john.desro@gmail.com>
2019-01-21 04:43:11 -0800

    SP: test for wrong input type

---
+++ stock-prices/stock-prices.js
@@ -0,0 +1,5 @@
+function buySellPriceForPrices (prices) {
+  throw new Error(`Expected prices to be an array, but got: ${prices}`)
+}
+
+module.exports = { buySellPriceForPrices }
--- stock-prices/test.js
+++ stock-prices/test.js
@@ -1,1 +1,19 @@
-describe('Stock Prices Kata', () => {})
+const should = require('chai').should()
+
+const { buySellPriceForPrices } = require('./stock-prices')
+
+describe('Stock Prices Kata', () => {
+  it('should throw with "Expected prices to be an array but got:..."', () => {
+    const expectedErrorMessage = pricesStr =>
+      `Expected prices to be an array, but got: ${pricesStr}`
+    should.throw(
+      () => buySellPriceForPrices(),
+      expectedErrorMessage('undefined')
+    )
+
+    should.throw(
+      () => buySellPriceForPrices('hello'),
+      expectedErrorMessage('hello')
+    )
+  })
+})
