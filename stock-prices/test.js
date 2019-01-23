4f1d1ef1f05fa1182e4c6ed2fbe90f54a1f5df4d
John Desrosiers <john.desro@gmail.com>
2019-01-23 04:54:12 -0800

    SP: work in progress
    This morning, I set out to solve this problem using es6 class syntax.
    The solution will not be so different than the previous single function
    solution from my previous practices. It takes a bit more code to
    test and to write using the class syntax which takes more time. I don't
    see any real benefit to using class syntax for the solution. I'm stopping
    here. Tomorrow, I'll do the practice with a functional syntax again.
    I'm thinking adding some constraints around input coming in or specifying
    error conditions may be a better progression than trying a class
    approach.

--- stock-prices/test.js
+++ stock-prices/test.js
@@ -1,7 +1,33 @@
 require('chai').should()
 
+const StockPrices = require('./stock-prices')
+
 describe('Stock Prices Kata', () => {
-  describe('StockPrice', () => {
-    it('')
+  describe('StockPrices', () => {
+    const testCase = (prices, expectedBuy, expectedSell) => {
+      return describe(`prices = [${prices.join(', ')}]`, () => {
+        let stockPrices
+
+        beforeEach(() => {
+          stockPrices = new StockPrices(prices)
+        })
+
+        it(`should return ${expectedBuy} and for buyPrice`, () =>
+          stockPrices.buyPrice().should.equal(expectedBuy))
+
+        it(`should return ${expectedSell} for sellPrice`, () =>
+          stockPrices.sellPrice().should.equal(expectedSell))
+      })
+    }
+    describe('two prices', () => {
+      testCase([1, 2], 1, 2)
+      testCase([2, 3], 2, 3)
+    })
+
+    describe('three prices', () => {
+      testCase([1, 2, 3], 1, 3)
+      testCase([2, 1, 3], 1, 3)
+      testCase([2, 3, 1], 2, 3)
+    })
   })
 })
