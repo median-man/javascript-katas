57fbae6682c79c54a72c535ff05d6870e627a54a
John Desrosiers <john.desro@gmail.com>
2019-02-02 05:25:11 -0800

    VS: Create master test and lint

--- video-store/statment.js
+++ video-store/statment.js
@@ -1,43 +1,44 @@
-/* eslint-disable */
-function statement(customer, movies) {
-  let totalAmount = 0;
-  let frequentRenterPoints = 0;
-  let result = `Rental Record for ${customer.name}\n`;
+function statement (customer, movies) {
+  let totalAmount = 0
+  let frequentRenterPoints = 0
+  let result = `Rental Record for ${customer.name}\n`
   for (let r of customer.rentals) {
-    let movie = movies[r.movieID];
-    let thisAmount = 0;
+    let movie = movies[r.movieID]
+    let thisAmount = 0
 
     // determine amount for each movie
     switch (movie.code) {
-      case "regular":
-        thisAmount = 2;
+      case 'regular':
+        thisAmount = 2
         if (r.days > 2) {
-          thisAmount += (r.days - 2) * 1.5;
+          thisAmount += (r.days - 2) * 1.5
         }
-        break;
-      case "new":
-        thisAmount = r.days * 3;
-        break;
-      case "childrens":
-        thisAmount = 1.5;
+        break
+      case 'new':
+        thisAmount = r.days * 3
+        break
+      case 'childrens':
+        thisAmount = 1.5
         if (r.days > 3) {
-          thisAmount += (r.days - 3) * 1.5;
+          thisAmount += (r.days - 3) * 1.5
         }
-        break;
+        break
     }
 
-    //add frequent renter points
-    frequentRenterPoints++;
+    // add frequent renter points
+    frequentRenterPoints++
     // add bonus for a two day new release rental
-    if(movie.code === "new" && r.days > 2) frequentRenterPoints++;
+    if (movie.code === 'new' && r.days > 2) frequentRenterPoints++
 
-    //print figures for this rental
-    result += `\t${movie.title}\t${thisAmount}\n` ;
-    totalAmount += thisAmount;
+    // print figures for this rental
+    result += `\t${movie.title}\t${thisAmount}\n`
+    totalAmount += thisAmount
   }
   // add footer lines
-  result += `Amount owed is ${totalAmount}\n`;
-  result += `You earned ${frequentRenterPoints} frequent renter points\n`;
+  result += `Amount owed is ${totalAmount}\n`
+  result += `You earned ${frequentRenterPoints} frequent renter points\n`
 
-  return result;
+  return result
 }
+
+module.exports = { statement }
--- video-store/test.js
+++ video-store/test.js
@@ -1,5 +1,31 @@
 require('chai').should()
 
+const { statement } = require('./statment')
+
 describe('video store kata', () => {
-  describe('statement', () => {})
+  describe('statement', () => {
+    it('master test prints statement', () => {
+      const customer = {
+        name: 'john',
+        rentals: [{ movieID: 'F001', days: 3 }, { movieID: 'F002', days: 1 }]
+      }
+
+      const movies = {
+        F001: { title: 'Ran', code: 'regular' },
+        F002: { title: 'Trois Couleurs: Bleu', code: 'regular' }
+        // etc
+      }
+
+      const expectedStatment = [
+        'Rental Record for john',
+        '\tRan\t3.5',
+        '\tTrois Couleurs: Bleu\t2',
+        'Amount owed is 5.5',
+        'You earned 2 frequent renter points',
+        ''
+      ].join('\n')
+
+      statement(customer, movies).should.equal(expectedStatment)
+    })
+  })
 })
