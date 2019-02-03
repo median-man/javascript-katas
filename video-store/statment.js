72da4b81d15b1b34d99ee521c30d415c206c62b3
John Desrosiers <john.desro@gmail.com>
2019-02-03 06:40:48 -0800

    Reset video store

--- video-store/statment.js
+++ video-store/statment.js
@@ -1,52 +1,42 @@
-function statement (customer, movies) {
-  let totalAmount = 0
-  let frequentRenterPoints = 0
-  let result = `Rental Record for ${customer.name}\n`
+function statement(customer, movies) {
+  let totalAmount = 0;
+  let frequentRenterPoints = 0;
+  let result = `Rental Record for ${customer.name}\n`;
   for (let r of customer.rentals) {
-    let thisAmount = amountFor(r)
-
-    // add frequent renter points
-    frequentRenterPoints++
-    // add bonus for a two day new release rental
-    if (movieFor(r).code === 'new' && r.days > 2) frequentRenterPoints++
-
-    // print figures for this rental
-    result += `\t${movieFor(r).title}\t${thisAmount}\n`
-    totalAmount += thisAmount
-  }
-  // add footer lines
-  result += `Amount owed is ${totalAmount}\n`
-  result += `You earned ${frequentRenterPoints} frequent renter points\n`
-
-  return result
-
-  function movieFor (r) {
-    return movies[r.movieID]
-  }
-
-  function amountFor (r) {
-    let amount = 0
+    let movie = movies[r.movieID];
+    let thisAmount = 0;
 
     // determine amount for each movie
-    switch (movieFor(r).code) {
-      case 'regular':
-        amount = 2
+    switch (movie.code) {
+      case "regular":
+        thisAmount = 2;
         if (r.days > 2) {
-          amount += (r.days - 2) * 1.5
+          thisAmount += (r.days - 2) * 1.5;
         }
-        break
-      case 'new':
-        amount = r.days * 3
-        break
-      case 'childrens':
-        amount = 1.5
+        break;
+      case "new":
+        thisAmount = r.days * 3;
+        break;
+      case "childrens":
+        thisAmount = 1.5;
         if (r.days > 3) {
-          amount += (r.days - 3) * 1.5
+          thisAmount += (r.days - 3) * 1.5;
         }
-        break
+        break;
     }
-    return amount
-  }
-}
 
-module.exports = { statement }
+    //add frequent renter points
+    frequentRenterPoints++;
+    // add bonus for a two day new release rental
+    if(movie.code === "new" && r.days > 2) frequentRenterPoints++;
+
+    //print figures for this rental
+    result += `\t${movie.title}\t${thisAmount}\n` ;
+    totalAmount += thisAmount;
+  }
+  // add footer lines
+  result += `Amount owed is ${totalAmount}\n`;
+  result += `You earned ${frequentRenterPoints} frequent renter points\n`;
+
+  return result;
+}
