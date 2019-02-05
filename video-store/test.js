1b27e7a7961d6b18340e492f433744ef11aa45b4
John Desrosiers <john.desro@gmail.com>
2019-02-05 06:09:43 -0800

    VS: Create test case for plain text statement

--- video-store/test.js
+++ video-store/test.js
@@ -4,4 +4,16 @@ const customer = require('./customer')
 const movies = require('./movies')
 const { statement } = require('./statement')
 
-describe('video store kata', () => {})
+describe('video store kata', () => {
+  describe('statement', () => {
+    it('should return a plain text statement', () => {
+      let expectedStatement = 'Rental Record for john\n'
+      expectedStatement += '\tA New Hope\t2\n'
+      expectedStatement += '\tThe Empire Strikes Back\t9\n'
+      expectedStatement += '\tReturn of the Jedi\t3\n'
+      expectedStatement += 'Amount owed is 14\n'
+      expectedStatement += 'You earned 4 frequent renter points\n'
+      statement(customer, movies).should.eql(expectedStatement)
+    })
+  })
+})
