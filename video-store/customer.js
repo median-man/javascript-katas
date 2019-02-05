f0ee9af4f88631b9fc6437916a2c1ebe7646dd87
John Desrosiers <john.desro@gmail.com>
2019-02-05 06:01:07 -0800

    Reset video store

---
+++ video-store/customer.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'john',
+  rentals: [
+    { movieID: 'F001', days: 2 },
+    { movieID: 'F002', days: 3 },
+    { movieID: 'F003', days: 4 }
+  ]
+}
---
+++ video-store/movies.js
@@ -0,0 +1,5 @@
+module.exports = {
+  F001: { title: 'A New Hope', code: 'regular' },
+  F002: { title: 'The Empire Strikes Back', code: 'new' },
+  F003: { title: 'Return of the Jedi', code: 'childrens' }
+}
