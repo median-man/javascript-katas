1adba93a399b6623ed76979198f727b686843038
John Desrosiers <john.desro@gmail.com>
2019-02-07 06:48:08 -0800

    VS: rename function - frequentRenterPoints()

--- video-store/statement.js
+++ video-store/statement.js
@@ -1,6 +1,5 @@
 function statement (customer, movies) {
   let totalAmount = 0
-  let frequentRenterPoints = 0
   let result = `Rental Record for ${customer.name}\n`
   for (let r of customer.rentals) {
     result += `\t${movieFor(r).title}\t${amountFor(r)}\n`
@@ -10,11 +9,11 @@ function statement (customer, movies) {
   }
   // add footer lines
   result += `Amount owed is ${totalAmount}\n`
-  result += `You earned ${appleSauce()} frequent renter points\n`
+  result += `You earned ${frequentRenterPoints()} frequent renter points\n`
 
   return result
 
-  function appleSauce () {
+  function frequentRenterPoints () {
     let result = 0
     for (let r of customer.rentals) {
       result += frequentRenterPointsFor(r)
