aa9621699caef470db5baf101eef46891e99c368
John Desrosiers <john.desro@gmail.com>
2019-02-07 06:48:37 -0800

    VS: slide statement - totalAmount

--- video-store/statement.js
+++ video-store/statement.js
@@ -1,9 +1,9 @@
 function statement (customer, movies) {
-  let totalAmount = 0
   let result = `Rental Record for ${customer.name}\n`
   for (let r of customer.rentals) {
     result += `\t${movieFor(r).title}\t${amountFor(r)}\n`
   }
+  let totalAmount = 0
   for (let r of customer.rentals) {
     totalAmount += amountFor(r)
   }
