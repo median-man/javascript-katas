edce9cbfa7b9b7716543a2b3ed306c53c7bd4086
John Desrosiers <john.desro@gmail.com>
2019-02-07 06:10:05 -0800

    Reset video store

--- video-store/statement.js
+++ video-store/statement.js
@@ -1,87 +1,42 @@
-function renderPlainText (customer, movies) {
-  const data = statementData(customer, movies)
-  let result = `Rental Record for ${customer.name}\n`
-  for (let r of data.rentals) {
-    result += `\t${r.title}\t${r.amount}\n`
-  }
-  result += `Amount owed is ${data.totalAmount}\n`
-  result += `You earned ${data.frequentRenterPoints} frequent renter points\n`
+function statement(customer, movies) {
+  let totalAmount = 0;
+  let frequentRenterPoints = 0;
+  let result = `Rental Record for ${customer.name}\n`;
+  for (let r of customer.rentals) {
+    let movie = movies[r.movieID];
+    let thisAmount = 0;
 
-  return result
-}
-
-function renderHtml (customer, movies) {
-  const data = statementData(customer, movies)
-  let result = `<h1>Rental Record for <em>${customer.name}</em></h1>\n`
-  result += '<table>\n'
-  result += '  <tr><th>Title</th><th>Days</th><th>Amount</th></tr>\n'
-  result += data.rentals.reduce(rentalLine, '')
-  result += `</table>\n`
-  result += `<p>Amount owed is <em>${data.totalAmount}</em></p>\n`
-  result += `<p>You earned <em>${
-    data.frequentRenterPoints
-  }</em> frequent renter points</p>\n`
-
-  return result
-
-  function rentalLine (lines, rental) {
-    return `${lines}  <tr><td>${rental.title}</td><td>${rental.days}</td><td>${
-      rental.amount
-    }</td></tr>\n`
-  }
-}
-
-function statementData (customer, movies) {
-  return {
-    totalAmount: totalAmount(),
-    frequentRenterPoints: frequentRenterPoints(),
-    rentals: customer.rentals.map(r => ({
-      title: movieFor(r).title,
-      amount: amountFor(r),
-      days: r.days
-    }))
-  }
-
-  function frequentRenterPoints () {
-    return customer.rentals
-      .map(frequentRenterPointsFor)
-      .reduce((a, b) => a + b, 0)
-  }
-
-  function totalAmount () {
-    return customer.rentals.map(amountFor).reduce((a, b) => a + b, 0)
-  }
-
-  function frequentRenterPointsFor (r) {
-    return movieFor(r).code === 'new' && r.days > 2 ? 2 : 1
-  }
-
-  function amountFor (rental) {
-    let result = 0
-    switch (movieFor(rental).code) {
-      case 'regular':
-        result = 2
-        if (rental.days > 2) {
-          result += (rental.days - 2) * 1.5
+    // determine amount for each movie
+    switch (movie.code) {
+      case "regular":
+        thisAmount = 2;
+        if (r.days > 2) {
+          thisAmount += (r.days - 2) * 1.5;
         }
-        return result
-      case 'new':
-        result = rental.days * 3
-        return result
-      case 'childrens':
-        result = 1.5
-        if (rental.days > 3) {
-          result += (rental.days - 3) * 1.5
+        break;
+      case "new":
+        thisAmount = r.days * 3;
+        break;
+      case "childrens":
+        thisAmount = 1.5;
+        if (r.days > 3) {
+          thisAmount += (r.days - 3) * 1.5;
         }
-        return result
-      default:
-        return result
+        break;
     }
-  }
 
-  function movieFor (rental) {
-    return movies[rental.movieID]
+    //add frequent renter points
+    frequentRenterPoints++;
+    // add bonus for a two day new release rental
+    if(movie.code === "new" && r.days > 2) frequentRenterPoints++;
+
+    //print figures for this rental
+    result += `\t${movie.title}\t${thisAmount}\n` ;
+    totalAmount += thisAmount;
   }
+  // add footer lines
+  result += `Amount owed is ${totalAmount}\n`;
+  result += `You earned ${frequentRenterPoints} frequent renter points\n`;
+
+  return result;
 }
-
-module.exports = { statement: { renderPlainText, renderHtml } }
--- video-store/test.js
+++ video-store/test.js
@@ -5,27 +5,5 @@ const movies = require('./movies')
 const { statement } = require('./statement')
 
 describe('video store kata', () => {
-  describe('statement', () => {
-    it('should return a plain text statement', () => {
-      let expectedStatement = 'Rental Record for john\n'
-      expectedStatement += '\tA New Hope\t2\n'
-      expectedStatement += '\tThe Empire Strikes Back\t9\n'
-      expectedStatement += '\tReturn of the Jedi\t3\n'
-      expectedStatement += 'Amount owed is 14\n'
-      expectedStatement += 'You earned 4 frequent renter points\n'
-      statement.renderPlainText(customer, movies).should.eql(expectedStatement)
-    })
-    it('should return an html statement', () => {
-      let expectedStatement = '<h1>Rental Record for <em>john</em></h1>\n'
-      expectedStatement += '<table>\n'
-      expectedStatement += '  <tr><th>Title</th><th>Days</th><th>Amount</th></tr>\n'
-      expectedStatement += '  <tr><td>A New Hope</td><td>2</td><td>2</td></tr>\n'
-      expectedStatement += '  <tr><td>The Empire Strikes Back</td><td>3</td><td>9</td></tr>\n'
-      expectedStatement += '  <tr><td>Return of the Jedi</td><td>4</td><td>3</td></tr>\n'
-      expectedStatement += '</table>\n'
-      expectedStatement += '<p>Amount owed is <em>14</em></p>\n'
-      expectedStatement += '<p>You earned <em>4</em> frequent renter points</p>\n'
-      statement.renderHtml(customer, movies).should.eql(expectedStatement)
-    })
-  })
+  describe('statement', () => {})
 })
