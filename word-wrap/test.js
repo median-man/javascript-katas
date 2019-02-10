36777a8a7432ca9ca5de4cb04f13a56310d1a0df
John Desrosiers <john.desro@gmail.com>
2019-02-10 08:43:04 -0800

    wrap: Passing test for split word

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -3,9 +3,20 @@ require('chai').should()
 const { wrap } = require('./word-wrap')
 
 describe('word-wrap kata', () => {
-  describe('degenerate tests', () => {
+  describe('does not wrap', () => {
     it('should return empty string given "" and 1', () => {
       wrap('', 1).should.equal('')
+    })
+
+    it('should not wrap when string length >= than columns', () => {
+      wrap('galaxy', 6).should.equal('galaxy', 'length is equal')
+      wrap('galaxy', 7).should.equal('galaxy', 'length is greater')
+    })
+  })
+
+  describe('two lines', () => {
+    it('should wrap words longer than columns', () => {
+      wrap('galaxy', 5).should.equal('galax\ny')
     })
   })
 })
--- word-wrap/word-wrap.js
+++ word-wrap/word-wrap.js
@@ -1,5 +1,8 @@
-function wrap () {
-  return ''
+function wrap (s, columns) {
+  if (s.length <= columns) {
+    return s
+  }
+  return s.substr(0, columns) + '\n' + s.substr(columns)
 }
 
 module.exports = { wrap }
