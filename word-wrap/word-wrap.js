b70fcaedd05ea3f65187099fd92f14797bd5d02e
John Desrosiers <john.desro@gmail.com>
2019-02-10 09:20:23 -0800

    wrap: refactor to while loop

--- word-wrap/word-wrap.js
+++ word-wrap/word-wrap.js
@@ -4,15 +4,11 @@ function wrap (s, columns) {
   }
   let result = s.substr(0, columns)
   let rest = s.substr(columns)
-  if (rest.length <= columns) {
-    result += '\n' + rest
-    return result
+  while (rest.length) {
+    result += '\n' + rest.substr(0, columns)
+    rest = rest.substr(columns)
   }
-  result += '\n' + rest.substr(0, columns)
-  rest = rest.substr(columns)
-  result += '\n' + rest
   return result
-  // return s.substr(0, columns) + '\n' + s.substr(columns)
 }
 
 module.exports = { wrap }
