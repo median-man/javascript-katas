3ba7489a149d8269b9baab72df30f2bcf49d6b23
John Desrosiers <john.desro@gmail.com>
2019-02-10 08:35:24 -0800

    wrap: returns  given  and 1

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -1,5 +1,11 @@
+require('chai').should()
+
+const { wrap } = require('./word-wrap')
+
 describe('word-wrap kata', () => {
-  describe('wrap', () => {
-    it('')
+  describe('degenerate tests', () => {
+    it('should return empty string given "" and 1', () => {
+      wrap('', 1).should.equal('')
+    })
   })
 })
