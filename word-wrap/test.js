18c583968402fc845f6dd3ab71289cfa0a3fce1f
John Desrosiers <john.desro@gmail.com>
2019-02-10 06:27:25 -0800

    wrap: passing split on space

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -4,21 +4,31 @@ const { wrap } = require('./word-wrap')
 
 describe('word-wrap kata', () => {
   describe('wrap', () => {
-    it('should throw when first argument is not a string', () => {
-      should.throw(() => wrap(0))
+    describe('degenerate cases', () => {
+      it('should throw when first argument is not a string', () => {
+        should.throw(() => wrap(0))
+      })
     })
 
-    it('should return an empty string given an empty string', () => {
-      wrap('', 1).should.equal('')
+    describe('no line breaks', () => {
+      it('should return an empty string given an empty string', () => {
+        wrap('', 1).should.equal('')
+      })
+
+      it('should return a single line when line length <= columns', () => {
+        const s = 'A long time ago in a galaxy far, far away....'
+        wrap(s, 45).should.equal(s)
+      })
     })
 
-    it('should return a single line when line length <= columns', () => {
-      const s = 'A long time ago in a galaxy far, far away....'
-      wrap(s, 45).should.equal(s)
-    })
+    describe('one line break', () => {
+      it('should split when columns is equal to word length', () => {
+        wrap('long time', 4).should.equal('long\ntime')
+      })
 
-    it('should split two words', () => {
-      wrap('long time', 4).should.equal('long\ntime')
+      it('should split when columns is equal to index of a space boundary', () => {
+        wrap('long time', 5).should.equal('long\ntime')
+      })
     })
   })
 })
--- word-wrap/word-wrap.js
+++ word-wrap/word-wrap.js
@@ -6,7 +6,12 @@ function wrap (s, columns) {
     return s
   }
   const LINE_BREAK = '\n'
-  return s.substr(0, columns) + LINE_BREAK + s.substr(columns + 1)
+  if (s[columns] === ' ') {
+    return s.substr(0, columns) + LINE_BREAK + s.substr(columns + 1)
+  }
+  if (s[columns - 1] === ' ') {
+    return s.substr(0, columns - 1) + LINE_BREAK + s.substr(columns)
+  }
 }
 
 module.exports = { wrap }
