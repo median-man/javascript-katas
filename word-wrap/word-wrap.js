3c23be305095dd0ae406f0a8583c9184f076e4ed
John Desrosiers <john.desro@gmail.com>
2019-02-11 05:36:46 -0800

    wrap: refactoring recursion to while loop

--- word-wrap/word-wrap.js
+++ word-wrap/word-wrap.js
@@ -2,7 +2,15 @@ function wrap (s, cols) {
   if (s.length <= cols) {
     return s
   }
-  return s.substr(0, cols) + '\n' + wrap(s.substr(cols), cols)
+  let rest = s
+  let result = ''
+  while (rest.length > cols) {
+    const line = rest.substr(0, cols)
+    rest = rest.substr(cols)
+    result += line + '\n'
+  }
+  result += rest
+  return result
 }
 
 module.exports = { wrap }
