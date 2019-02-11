aa965128afc1278fe4042a6b25c785fadd0bc4e7
John Desrosiers <john.desro@gmail.com>
2019-02-11 05:21:31 -0800

    Reset word-wrap kata

--- word-wrap/readme.md
+++ word-wrap/readme.md
@@ -1,25 +1,20 @@
 # Word Wrap
 
-This kata is based upon
-[this blog post](http://thecleancoder.blogspot.com/2010/10/craftsman-62-dark-path.html).
+This kata is based upon (this blog
+post)[http://thecleancoder.blogspot.com/2010/10/craftsman-62-dark-path.html].
 
 Create a function named `wrap` that takes two arguments: a string and a column
 number. The function should return a string with line breaks inserted so that no
 line is longer than the given column number. Lines should break at word
-boundaries. Wrap should break a word only if the word has a length that is
-longer than the given columns. Treat punctuation marks as part of the adjacent
-word.
+boundaries.
 
 ## Example:
-
 Input:
-
 ```
 'A long time ago in a galaxy far, far away....'
 ```
 
-Result of `wrap(input, 7)`:
-
+Result of `wrap(input, 10)`:
 ```
-'A long\ntime\nago in\na\ngalaxy\nfar,\nfar\naway...\n.'
+'A long\ntime ago\nin a\ngalaxy\nfar, far\naway....'
 ```
--- word-wrap/test.js
+++ word-wrap/test.js
@@ -1,40 +1,5 @@
-require('chai').should()
-
-const { wrap } = require('./word-wrap')
-
 describe('word-wrap kata', () => {
-  describe('does not wrap', () => {
-    it('should return empty string given "" and 1', () => {
-      wrap('', 1).should.equal('')
-    })
-
-    it('should not wrap when string length >= than columns', () => {
-      wrap('galaxy', 6).should.equal('galaxy', 'length is equal')
-      wrap('galaxy', 7).should.equal('galaxy', 'length is greater')
-    })
-  })
-
-  describe('no spaces', () => {
-    it('should split a single word in two', () => {
-      wrap('galaxy', 5).should.equal('galax\ny')
-    })
-
-    it('should split a single word in three', () => {
-      wrap('galaxy', 2).should.equal('ga\nla\nxy')
-    })
-  })
-
-  describe('one space', () => {
-    it('should split when columns is at space', () => {
-      wrap('far far', 4).should.equal('far\nfar')
-    })
-
-    it('should split when columns is after space', () => {
-      wrap('far far', 5).should.equal('far\nfar')
-    })
-
-    it('should split when columns is before space', () => {
-      wrap('far far', 2).should.equal('fa\nr\nfa\nr')
-    })
+  describe('wrap', () => {
+    it('')
   })
 })
