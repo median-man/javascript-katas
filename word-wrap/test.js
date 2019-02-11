689bb300b0842487da37d35273cf8e9adbad2e06
John Desrosiers <john.desro@gmail.com>
2019-02-11 05:28:37 -0800

    wrap: passing test to return one line

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -8,6 +8,10 @@ describe('word-wrap kata', () => {
       it('should return an empty string given s == "" and cols = 1', () => {
         wrap('', 1).should.equal('')
       })
+
+      it('should return one line give s is shorter than cols', () => {
+        wrap('galaxy', 10).should.equal('galaxy')
+      })
     })
   })
 })
