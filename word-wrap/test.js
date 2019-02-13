ef272a1171508b994f20731a91d0b7a54da67d26
John Desrosiers <john.desro@gmail.com>
2019-02-13 04:46:49 -0800

    Reset word wrap kata

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -1,44 +1,5 @@
-require('chai').should()
-
-const { wrap } = require('./word-wrap')
-
 describe('word-wrap kata', () => {
   describe('wrap', () => {
-    describe('degenerate tests', () => {
-      it('should return an empty string given s == "" and cols = 1', () => {
-        wrap('', 1).should.equal('')
-      })
-
-      it('should return one line give s is shorter than cols', () => {
-        wrap('galaxy', 10).should.equal('galaxy')
-      })
-    })
-    describe('split word tests', () => {
-      it('should split a word in two', () => {
-        wrap('galaxy', 3).should.equal('gal\naxy')
-      })
-
-      it('should split a word into multiple lines', () => {
-        wrap('galaxy', 2).should.equal('ga\nla\nxy')
-      })
-    })
-
-    describe('wrap two words', () => {
-      it('should split when columns is at word boundary', () => {
-        wrap('far far', 4).should.equal('far\nfar')
-      })
-
-      it('should split when columns is after word boundary', () => {
-        wrap('far far', 5).should.equal('far\nfar')
-      })
-
-      it('should split when columns splits first word', () => {
-        wrap('far far', 2).should.equal('fa\nr\nfa\nr')
-      })
-
-      it('should split when columns is at end of first word', () => {
-        wrap('far far', 3).should.equal('far\nfar')
-      })
-    })
+    it('')
   })
 })
