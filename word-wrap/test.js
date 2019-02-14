ee0d14607d68bba2923782a29aaaa8bb80b838eb
John Desrosiers <john.desro@gmail.com>
2019-02-14 04:17:24 -0800

    Reset word wrap kata

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -1,25 +1,5 @@
-const { wrap } = require('./word-wrap')
-
 describe('word-wrap kata', () => {
   describe('wrap', () => {
-    describe('degenerate tests', () => {
-      it('should return empty string given an empty string', () => {
-        wrap('', 1).should.equal('')
-      })
-
-      it('should not wrap given string with length < cols', () => {
-        wrap('galaxy', 7).should.equal('galaxy')
-      })
-    })
-
-    describe('split word tests', () => {
-      it('should split a word with length greater than cols', () => {
-        wrap('galaxy', 5).should.equal('galax\ny')
-      })
-
-      it('should split a word multiple times', () => {
-        wrap('galaxy', 2).should.equal('ga\nla\nxy')
-      })
-    })
+    it('')
   })
 })
