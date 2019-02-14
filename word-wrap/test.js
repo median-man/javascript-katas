931515e17d03f44cdfe2fb4ca4567fe2c097f6ef
John Desrosiers <john.desro@gmail.com>
2019-02-14 04:26:22 -0800

    wrap: pass test 3 (split word)

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -4,13 +4,18 @@ const { wrap } = require('./word-wrap')
 
 describe('word-wrap kata', () => {
   describe('wrap', () => {
-    describe('degnerate tests', () => {
+    describe('degenerate tests', () => {
       it('should return empty string given an empty string', () => {
         wrap('', 1).should.equal('')
       })
 
       it('should not split a string shorter than given cols', () => {
         wrap('galaxy far, far', 16).should.equal('galaxy far, far')
+      })
+    })
+    describe('single word tests', () => {
+      it('should split a single word', () => {
+        wrap('galaxy', 5).should.equal('galax\ny')
       })
     })
   })
