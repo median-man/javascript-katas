c6101e496534550814072206c41d2a97ea55651e
John Desrosiers <john.desro@gmail.com>
2019-02-14 04:40:56 -0800

    wrap: refactoring - extract boundaryAt var

--- word-wrap/word-wrap.js
+++ word-wrap/word-wrap.js
@@ -2,9 +2,10 @@ function wrap (s, cols) {
   let rest = s
   let result = ''
   while (rest.length > cols) {
-    if (rest.lastIndexOf(' ', cols) > -1) {
-      result += rest.substr(0, rest.lastIndexOf(' ', cols)) + '\n'
-      rest = rest.substr(rest.lastIndexOf(' ', cols) + 1)
+    const boundaryAt = rest.lastIndexOf(' ', cols)
+    if (boundaryAt > -1) {
+      result += rest.substr(0, boundaryAt) + '\n'
+      rest = rest.substr(boundaryAt + 1)
     } else {
       result += rest.substr(0, cols) + '\n'
       rest = rest.substr(cols)
