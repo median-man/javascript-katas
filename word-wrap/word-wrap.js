8ae4ebe88822846dc2b80221a40ed5110709e4ff
John Desrosiers <john.desro@gmail.com>
2019-02-14 04:48:23 -0800

    wrap: refactoring - extract functions

--- word-wrap/word-wrap.js
+++ word-wrap/word-wrap.js
@@ -1,18 +1,24 @@
 function wrap (s, cols) {
   let rest = s
   let result = ''
-  while (rest.length > cols) {
+  while (restIsLongerThanCols()) {
     const boundaryAt = rest.lastIndexOf(' ', cols)
     if (boundaryAt > -1) {
-      result += rest.substr(0, boundaryAt) + '\n'
-      rest = rest.substr(boundaryAt + 1)
+      breakLine(boundaryAt, 1)
     } else {
-      result += rest.substr(0, cols) + '\n'
-      rest = rest.substr(cols)
+      breakLine(cols, 0)
     }
   }
   result += rest
   return result
+
+  function restIsLongerThanCols () {
+    return rest.length > cols
+  }
+  function breakLine (breakAt, gap) {
+    result += rest.substr(0, breakAt) + '\n'
+    rest = rest.substr(breakAt + gap)
+  }
 }
 
 module.exports = { wrap }
