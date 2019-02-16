42e0c7d674fd07826e30bb60cf834a5445c24a8e
John Desrosiers <john.desro@gmail.com>
2019-02-16 05:40:29 -0800

    wrap: returns empty string

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -1,7 +1,17 @@
 require('chai').should()
 
+const { wrap } = require('./word-wrap')
+
 describe('word-wrap kata', () => {
   describe('wrap', () => {
-    it('')
+    describe('degenerate tests', () => {
+      it('should return an empty string given an empty string', () => {
+        wrap('', 1).should.equal('')
+      })
+
+      // it('should return an empty string given an empty string', () => {
+      //   wrap('', 1).should.equal('')
+      // })
+    })
   })
 })
