9e99d07ff772e7484c15aa96bc999be5e764ed7c
John Desrosiers <john.desro@gmail.com>
2019-02-16 05:57:29 -0800

    wrap: refactoring

--- word-wrap/word-wrap.js
+++ word-wrap/word-wrap.js
@@ -2,11 +2,11 @@ function wrap (s, cols) {
   let rest = s
   let result = ''
   while (rest.length > cols) {
+    let gap = 0
     if (rest[cols - 1] === ' ') {
-      result += rest.substr(0, cols - 1) + '\n'
-    } else {
-      result += rest.substr(0, cols) + '\n'
+      gap = 1
     }
+    result += rest.substr(0, cols - gap) + '\n'
     rest = rest.substr(cols)
   }
   result += rest
