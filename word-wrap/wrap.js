37d78a1ddd5c4b4d267ed2c8e10e78bd6343c95c
John Desrosiers <john.desro@gmail.com>
2019-02-18 06:00:03 -0800

    wrap: refactoring

--- word-wrap/wrap.js
+++ word-wrap/wrap.js
@@ -2,13 +2,19 @@ function wrap (s, cols) {
   if (s.length <= cols) {
     return s
   }
-  if (s.lastIndexOf(' ') > -1) {
-    return s.substr(0, s.lastIndexOf(' ')) + lineBreak() + wrap(s.substr(s.lastIndexOf(' ') + 1), cols)
-  }
-  if (s[cols - 1] === ' ') {
-    return s.substr(0, cols - 1) + lineBreak() + wrap(s.substr(cols), cols)
+  const boundaryAt = s.lastIndexOf(wordBoundary())
+  if (boundaryAt > -1) {
+    return (
+      s.substr(0, boundaryAt) +
+      lineBreak() +
+      wrap(s.substr(boundaryAt + 1), cols)
+    )
   }
   return s.substr(0, cols) + lineBreak() + wrap(s.substr(cols), cols)
+}
+
+function wordBoundary () {
+  return ' '
 }
 
 function lineBreak () {
