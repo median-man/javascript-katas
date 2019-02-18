e7c802382fc17708251a7518e188bf81d32298f7
John Desrosiers <john.desro@gmail.com>
2019-02-18 05:58:05 -0800

    wrap: pass test 6: cols after boundary

--- word-wrap/wrap.js
+++ word-wrap/wrap.js
@@ -2,6 +2,9 @@ function wrap (s, cols) {
   if (s.length <= cols) {
     return s
   }
+  if (s.lastIndexOf(' ') > -1) {
+    return s.substr(0, s.lastIndexOf(' ')) + lineBreak() + wrap(s.substr(s.lastIndexOf(' ') + 1), cols)
+  }
   if (s[cols - 1] === ' ') {
     return s.substr(0, cols - 1) + lineBreak() + wrap(s.substr(cols), cols)
   }
