e63aa0a799f272f3b09557563c801c8ed0478e85
John Desrosiers <john.desro@gmail.com>
2019-02-18 05:50:15 -0800

    wrap: pass test 4: split word twice

--- word-wrap/wrap.js
+++ word-wrap/wrap.js
@@ -1,6 +1,6 @@
 function wrap (s, cols) {
   if (s.length > cols) {
-    return s.substr(0, cols) + lineBreak() + s.substr(cols)
+    return s.substr(0, cols) + lineBreak() + wrap(s.substr(cols), cols)
   }
   return s
 }
