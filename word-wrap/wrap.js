461fbe903a4c20328ec9ee29087be201e08e2e7c
John Desrosiers <john.desro@gmail.com>
2019-02-18 05:48:51 -0800

    wrap: refactoring

--- word-wrap/wrap.js
+++ word-wrap/wrap.js
@@ -1,8 +1,12 @@
 function wrap (s, cols) {
   if (s.length > cols) {
-    return s.substr(0, cols) + '\n' + s.substr(cols)
+    return s.substr(0, cols) + lineBreak() + s.substr(cols)
   }
   return s
+}
+
+function lineBreak () {
+  return '\n'
 }
 
 module.exports = { wrap }
