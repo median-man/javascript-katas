4d2fdb494d88a48b0e13faef27ecf0331f609d7a
John Desrosiers <john.desro@gmail.com>
2019-02-19 04:56:46 -0800

    wrap: Add test 2 - no wrap (red)

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -8,6 +8,10 @@ describe('word-wrap kata', () => {
       it('should return an empty string', () => {
         wrap('', 1).should.equal('')
       })
+
+      it('should not wrap given cols > string length', () => {
+        wrap('galaxy far, far away', 21).should.equal('galaxy far, far away')
+      })
     })
   })
 })
