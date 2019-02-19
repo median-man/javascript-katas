fbc18e96d819e21f446f2a68e1dbb798b86433bf
John Desrosiers <john.desro@gmail.com>
2019-02-19 04:53:07 -0800

    wrap: Reset word-wrap kata

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -1,41 +1,7 @@
 require('chai').should()
 
-const { wrap } = require('./wrap')
-
 describe('word-wrap kata', () => {
   describe('wrap', () => {
-    describe('degenerate tests', () => {
-      it('should return an empty string', () => {
-        wrap('', 1).should.equal('')
-      })
-
-      it('should not wrap string given cols > length', () => {
-        wrap('galaxy far, far away', 21).should.equal('galaxy far, far away')
-      })
-    })
-
-    describe('single word tests', () => {
-      it('should split word', () => {
-        wrap('galaxy', 5).should.equal('galax\ny')
-      })
-
-      it('should split word twice', () => {
-        wrap('galaxy', 2).should.equal('ga\nla\nxy')
-      })
-    })
-
-    describe('word boundary tests', () => {
-      it('should split when cols is on word boundary', () => {
-        wrap('galaxy far', 7).should.equal('galaxy\nfar')
-      })
-
-      it('should split when cols is after boundary', () => {
-        wrap('galaxy far', 8).should.equal('galaxy\nfar')
-      })
-
-      it('should split several times', () => {
-        wrap('galaxy far', 3).should.equal('gal\naxy\nfar')
-      })
-    })
+    it('', () => {})
   })
 })
