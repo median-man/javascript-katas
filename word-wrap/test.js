247cf73f3ca5383afdae12cb239ddacfaf27da15
John Desrosiers <john.desro@gmail.com>
2019-02-20 06:19:33 -0800

    wrap: add test 3: split word once (red)

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -13,5 +13,13 @@ describe('word-wrap kata', () => {
         wrap('galaxy far, far away', 29).should.equal('galaxy far, far away')
       })
     })
+
+    describe('single word tests', () => {
+      it('should split word once', () => {
+        wrap('galaxy', 5).should.equal('galax\ny')
+      })
+
+      it('should ...', () => {})
+    })
   })
 })
