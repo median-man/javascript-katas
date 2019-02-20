5c672868632eab5e13b76e0bc1f3f87061518d6b
John Desrosiers <john.desro@gmail.com>
2019-02-20 06:17:09 -0800

    wrap: pass first test: empty string (green)

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -1,7 +1,13 @@
+const { wrap } = require('./wrap')
+
 require('chai').should()
 
 describe('word-wrap kata', () => {
   describe('wrap', () => {
-    it('', () => {})
+    describe('degenerate tests', () => {
+      it('should return empty string', () => {
+        wrap('', 1).should.equal('')
+      })
+    })
   })
 })
