0e83c4b345ce9c7953c435fb2525f7a147f9decb
John Desrosiers <john.desro@gmail.com>
2019-02-20 06:22:21 -0800

    wrap: add test 4: split word twice (red)

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -18,8 +18,10 @@ describe('word-wrap kata', () => {
       it('should split word once', () => {
         wrap('galaxy', 5).should.equal('galax\ny')
       })
-
-      it.skip('should ...', () => {})
+      
+      it('should split word twice', () => {
+        wrap('galaxy', 2).should.equal('ga\nla\nxy')
+      })
     })
   })
 })
