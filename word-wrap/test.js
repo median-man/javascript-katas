072deaa2d7d056d6839312f776a7f21a18be765c
John Desrosiers <john.desro@gmail.com>
2019-02-21 04:20:51 -0800

    wrap: pass test 2 - one line

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -7,6 +7,10 @@ describe('word-wrap kata', () => {
       it('should return an empty string', () => {
         wrap('', 1).should.equal('')
       })
+
+      it('should return a single line', () => {
+        wrap('galaxy far, far away', 20).should.equal('galaxy far, far away')
+      })
     })
   })
 })
