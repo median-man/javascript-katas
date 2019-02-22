9b9609f17a8f40cad209621ac722c04a5b03aeb8
John Desrosiers <john.desro@gmail.com>
2019-02-22 05:32:47 -0800

    wrap: breaks when cols at space (green)

--- word-wrap/wrap.js
+++ word-wrap/wrap.js
@@ -2,6 +2,9 @@ function wrap (s, cols) {
   if (s.length <= cols) {
     return s
   }
+  if (s[cols - 1] === ' ') {
+    return s.substr(0, cols - 1) + '\n' + wrap(s.substr(cols), cols)
+  }
   return s.substr(0, cols) + '\n' + wrap(s.substr(cols), cols)
 }
 exports.wrap = wrap
