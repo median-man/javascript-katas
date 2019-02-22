a5c9e3f292fd999febedd9075ba126a4c5a49e84
John Desrosiers <john.desro@gmail.com>
2019-02-22 05:40:09 -0800

    wrap: adds several breaks (green)

--- word-wrap/wrap.js
+++ word-wrap/wrap.js
@@ -2,7 +2,7 @@ function wrap (s, cols) {
   if (s.length <= cols) {
     return s
   }
-  const boundaryAt = s.lastIndexOf(' ')
+  const boundaryAt = s.lastIndexOf(' ', cols)
   const isBoundaryFound = boundaryAt > -1
   if (isBoundaryFound) {
     return s.substr(0, boundaryAt) + '\n' + wrap(s.substr(boundaryAt + 1), cols)
