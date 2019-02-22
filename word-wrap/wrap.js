7fcdfcf29032eda89b799e594a2ba65aa491a822
John Desrosiers <john.desro@gmail.com>
2019-02-22 05:35:51 -0800

    wrap: break when cols after space (green)

--- word-wrap/wrap.js
+++ word-wrap/wrap.js
@@ -2,6 +2,13 @@ function wrap (s, cols) {
   if (s.length <= cols) {
     return s
   }
+  if (s.lastIndexOf(' ') > -1) {
+    return (
+      s.substr(0, s.lastIndexOf(' ')) +
+      '\n' +
+      wrap(s.substr(s.lastIndexOf(' ') + 1), cols)
+    )
+  }
   if (s[cols - 1] === ' ') {
     return s.substr(0, cols - 1) + '\n' + wrap(s.substr(cols), cols)
   }
