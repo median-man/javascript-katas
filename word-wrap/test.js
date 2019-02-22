a8bf0fc47fd19714199eb399f40243f1437a55d0
John Desrosiers <john.desro@gmail.com>
2019-02-22 05:05:23 -0800

    Reset word-wrap kata

--- word-wrap/test.js
+++ word-wrap/test.js
@@ -1,40 +1,7 @@
-const { wrap } = require('./wrap')
-
 require('chai').should()
+
 describe('word-wrap kata', () => {
   describe('wrap', () => {
-    describe('degenerate tests', () => {
-      it('should return an empty string', () => {
-        wrap('', 1).should.equal('')
-      })
-
-      it('should return a single line', () => {
-        wrap('galaxy far, far away', 20).should.equal('galaxy far, far away')
-      })
-    })
-
-    describe('single word tests', () => {
-      it('should split once', () => {
-        wrap('galaxy', 5).should.equal('galax\ny')
-      })
-
-      it('should split twice', () => {
-        wrap('galaxy', 2).should.equal('ga\nla\nxy')
-      })
-    })
-
-    describe('word boundary tests', () => {
-      it('should split when cols at space', () => {
-        wrap('galaxy far', 7).should.equal('galaxy\nfar')
-      })
-
-      it('should split when cols is after space', () => {
-        wrap('galaxy far', 8).should.equal('galaxy\nfar')
-      })
-
-      it('should split when cols is before space', () => {
-        wrap('galaxy far', 6).should.equal('galaxy\nfar')
-      })
-    })
+    it('', () => {})
   })
 })
