b53359985a4f0f34519a357fd7a3ac39afdd7621
John Desrosiers <john.desro@gmail.com>
2019-02-23 04:56:49 -0800

    wrap: wraps when cols at space (green)

--- word-wrap/wrap.js
+++ word-wrap/wrap.js
@@ -3,8 +3,13 @@ function wrap (s, cols) {
   let result = ''
 
   while (rest.length > cols) {
-    result += rest.substr(0, cols) + '\n'
-    rest = rest.substr(cols)
+    if (rest[cols - 1] === ' ') {
+      result += rest.substr(0, cols - 1) + '\n'
+      rest = rest.substr(cols)
+    } else {
+      result += rest.substr(0, cols) + '\n'
+      rest = rest.substr(cols)
+    }
   }
   result += rest
   return result
